cfec5e6c89c746efe263f3e52f81fb2007674684
Ben Adams <thundercat@illyriad.co.uk>
2015-04-26 00:56:01 +0100

    Ensure ms is integer

--- JavaScript/JavaScriptSDK/Util.ts
+++ JavaScript/JavaScriptSDK/Util.ts
@@ -113,7 +113,7 @@
             if (isNaN(totalms) || totalms < 0) {
                 totalms = 0;
             }
-
+            totalms = Math.floor(totalms);
             var ms = "" + totalms % 1000;
             var sec = "" + Math.floor(totalms / 1000) % 60;
             var min = "" + Math.floor(totalms / (1000 * 60)) % 60;
