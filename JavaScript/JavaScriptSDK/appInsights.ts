ac70be5674d52c002af61a421ef66a1995bb3787
Max Shekhovtcov <Max.Shekhovtsov@microsoft.com>
2015-06-02 15:20:02 -0700

    bumping up sdk version

--- JavaScript/JavaScriptSDK/appInsights.ts
+++ JavaScript/JavaScriptSDK/appInsights.ts
@@ -6,7 +6,7 @@
 module Microsoft.ApplicationInsights {
     "use strict";
 
-    export var Version = "0.15.20150528.2";
+    export var Version = "0.15.20150602.8";
 
     export interface IConfig {
         instrumentationKey: string;
