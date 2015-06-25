dba33fbd3b513240929053ad3e1864c82e6ee7c5
Oleg Sych <olegsych@microsoft.com>
2015-06-25 15:01:19 -0700

    - Implement RunTestsInBrowser.ps1 script for running check-in tests interactively

---
+++ RunTestsInBrowser.ps1
@@ -0,0 +1,10 @@
+# This script starts IIS Express and opens the Tests.html in a browser to host QUnit test runner
+# - Run this script: > powershell "& .\RunTestsInBrowser.ps1"
+# - Build the solution after changing TypeScript source files to regenerate JavaScript 
+# - Refresh Tests.html in the browser to run the JavaScript tests 
+
+$iisExpress = Join-Path ${Env:ProgramFiles(x86)} "IIS Express\iisexpress.exe"
+$testsPath = Join-Path (Get-Location) "JavaScript\JavaScriptSDK.Tests\Selenium"
+$port = (Get-NetTCPConnection | Where-Object {$_.LocalAddress -eq "127.0.0.1" -or $_.LocalAddress -eq "::" } | Measure-Object -property LocalPort -max).Maximum + 1
+Start-Process "$iisExpress" -Args ("/path:$testsPath", "/port:$port")
+Start-Process "http://localhost:$port/Tests.html"
