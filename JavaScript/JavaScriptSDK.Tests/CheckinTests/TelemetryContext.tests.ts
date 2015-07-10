b451f224e6179a6613596cd4d6dcb84e75c6191b
Max Shekhovtsov <mashekho@microsoft.com>
2015-07-10 08:59:04 -0700

    refactored sampling tests

--- JavaScript/JavaScriptSDK.Tests/CheckinTests/TelemetryContext.tests.ts
+++ JavaScript/JavaScriptSDK.Tests/CheckinTests/TelemetryContext.tests.ts
@@ -75,58 +75,75 @@ class TelemetryContextTests extends TestClass {
             return new Microsoft.ApplicationInsights.Telemetry.Common.Envelope(data, envelopeType);
         }
         
-        this.testCase({
-            name: "TelemetryContext: some telemetry types get sampled and some do not",
-            test: () => {
-                var pageViewCallsCount = 0;
-                var eventCallsCount = 0;
-                var exceptionCallsCount = 0;
-                var metricCallsCount = 0;
-                var pageViewPerformanceCallsCount = 0;
-                var sessionCallsCount = 0;
-                var traceCallsCount = 0;
-                var isSampledInSpy = sinon.stub(this._telemetryContext.sample, "isSampledIn",
-                    (envelope: Microsoft.ApplicationInsights.Telemetry.Common.Envelope) => {
-                        switch (envelope.name) {
-                            case Microsoft.ApplicationInsights.Telemetry.PageView.envelopeType:
-                                pageViewCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.Event.envelopeType:
-                                eventCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.Exception.envelopeType:
-                                exceptionCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.Metric.envelopeType:
-                                metricCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.PageViewPerformance.envelopeType:
-                                pageViewPerformanceCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.SessionTelemetry.envelopeType:
-                                sessionCallsCount++;
-                                break;
-                            case Microsoft.ApplicationInsights.Telemetry.Trace.envelopeType:
-                                traceCallsCount++;
-                                break;
-                            default:
-                                break;
-                        }
-                    });
+        /**
+        * Gets the sinon stub for telemetryContext.sample.isSampledIn function. Result is wrapped to an object
+        * which has a counter of how many times the stub was accessed with expected envelope type.
+        */
+        function getStub(envelopeType: string, telemetryContext: Microsoft.ApplicationInsights.TelemetryContext) {            
+            var stub = {
+                sinonStub: null,
+                isSampledInCallsCount: 0
+            };
 
-                var pageViewEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.PageView>(
+            var isSampledInStub = sinon.stub(telemetryContext.sample, "isSampledIn",
+                (envelope: Microsoft.ApplicationInsights.Telemetry.Common.Envelope) => {
+                    if (envelope.name === envelopeType) {
+                        ++stub.isSampledInCallsCount;
+                    }
+                });
+
+            stub.sinonStub = isSampledInStub;
+
+            return stub;
+        }
+
+        this.testCase({
+            name: "TelemetryContext: page views get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.PageView.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.PageView>(
                     new Microsoft.ApplicationInsights.Telemetry.PageView("asdf", "asdf", 10),
                     Microsoft.ApplicationInsights.Telemetry.PageView.dataType,
                     Microsoft.ApplicationInsights.Telemetry.PageView.envelopeType);
-                this._telemetryContext.track(pageViewEnvelope);
-                Assert.equal(1, pageViewCallsCount);
 
-                var eventEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Event>(
+                // act
+                this._telemetryContext.track(envelope);
+
+                // assert
+                Assert.equal(1, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+
+        this.testCase({
+            name: "TelemetryContext: events get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.Event.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Event>(
                     new Microsoft.ApplicationInsights.Telemetry.Event("asdf"),
                     Microsoft.ApplicationInsights.Telemetry.Event.dataType,
                     Microsoft.ApplicationInsights.Telemetry.Event.envelopeType);
-                this._telemetryContext.track(eventEnvelope);                                
-                Assert.equal(1, eventCallsCount);
+                
+                // act
+                this._telemetryContext.track(envelope);
+
+                // assert
+                Assert.equal(1, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryContext: exceptions get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.Exception.envelopeType, this._telemetryContext);
 
                 var exception;
                 try {
@@ -135,44 +152,105 @@ class TelemetryContextTests extends TestClass {
                     exception = e;
                 }
 
-                var exceptionEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Exception>(
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Exception>(
                     new Microsoft.ApplicationInsights.Telemetry.Exception(exception),
                     Microsoft.ApplicationInsights.Telemetry.Exception.dataType,
                     Microsoft.ApplicationInsights.Telemetry.Exception.envelopeType);
-                this._telemetryContext.track(exceptionEnvelope);
-                Assert.equal(1, exceptionCallsCount);
+                
+                // act
+                this._telemetryContext.track(envelope);
 
-                var metricEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Metric>(
+                // assert
+                Assert.equal(1, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryContext: metrics do NOT get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.Metric.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Metric>(
                     new Microsoft.ApplicationInsights.Telemetry.Metric("asdf", 1234),
                     Microsoft.ApplicationInsights.Telemetry.Metric.dataType,
                     Microsoft.ApplicationInsights.Telemetry.Metric.envelopeType);
-                this._telemetryContext.track(metricEnvelope);
-                Assert.equal(0, metricCallsCount);
+                                
+                // act
+                this._telemetryContext.track(envelope);
 
-                var pageViewPerformanceEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.PageViewPerformance>(
+                // assert
+                Assert.equal(0, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryContext: pageViewPerformance gets sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.PageViewPerformance.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.PageViewPerformance>(
                     new Microsoft.ApplicationInsights.Telemetry.PageViewPerformance("adsf", "asdf", 10),
                     Microsoft.ApplicationInsights.Telemetry.PageViewPerformance.dataType,
                     Microsoft.ApplicationInsights.Telemetry.PageViewPerformance.envelopeType);
-                this._telemetryContext.track(pageViewPerformanceEnvelope);
-                Assert.equal(1, pageViewPerformanceCallsCount);
-                
-                var sessionEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.SessionTelemetry>(
+                                
+                // act
+                this._telemetryContext.track(envelope);
+
+                // assert
+                Assert.equal(1, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryContext: sessions do NOT get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.SessionTelemetry.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.SessionTelemetry>(
                     new Microsoft.ApplicationInsights.Telemetry.SessionTelemetry(AI.SessionState.Start),
                     Microsoft.ApplicationInsights.Telemetry.SessionTelemetry.dataType,
                     Microsoft.ApplicationInsights.Telemetry.SessionTelemetry.envelopeType);
-                this._telemetryContext.track(sessionEnvelope);
-                Assert.equal(0, sessionCallsCount);
+                                
+                // act
+                this._telemetryContext.track(envelope);
 
-                var traceEnvelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Trace>(
+                // assert
+                Assert.equal(0, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryContext: traces get sampled",
+            test: () => {
+                var stub = getStub(Microsoft.ApplicationInsights.Telemetry.Trace.envelopeType, this._telemetryContext);
+
+                var envelope = getEnvelope<Microsoft.ApplicationInsights.Telemetry.Trace>(
                     new Microsoft.ApplicationInsights.Telemetry.Trace("afd"),
                     Microsoft.ApplicationInsights.Telemetry.Trace.dataType,
                     Microsoft.ApplicationInsights.Telemetry.Trace.envelopeType);
-                this._telemetryContext.track(traceEnvelope);
-                Assert.equal(1, traceCallsCount);
+                                
+                // act
+                this._telemetryContext.track(envelope);
 
-                isSampledInSpy.restore();
+                // assert
+                Assert.equal(1, stub.isSampledInCallsCount);
+
+                // tear down
+                stub.sinonStub.restore();
             }
-        });
+        });     
     }
 }
 new TelemetryContextTests().registerTests();
