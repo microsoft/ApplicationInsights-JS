9b7bf8284c86596a248da8e299d7b781f960a6a8
Max Shekhovtsov <mashekho@microsoft.com>
2015-07-17 10:48:30 -0700

    porting ben's changes to accept optional Timings

--- JavaScript/JavaScriptSDK/Telemetry/PageViewPerformance.ts
+++ JavaScript/JavaScriptSDK/Telemetry/PageViewPerformance.ts
@@ -32,7 +32,7 @@ module Microsoft.ApplicationInsights.Telemetry {
         /**
          * Constructs a new instance of the PageEventTelemetry object
          */
-        constructor(name: string, url: string, durationMs: number, properties?: any, measurements?: any) {
+        constructor(name: string, url: string, durationMs: number, properties?: any, measurements?: any, timings?: Timings) {
             super();
 
             this.isValid = false;
@@ -49,43 +49,57 @@ module Microsoft.ApplicationInsights.Telemetry {
              *  |---network---||---request---|---response---|---dom---|
              *  |--------------------------total----------------------|
              */
-            var timing = PageViewPerformance.getPerformanceTiming();
-            if (timing) {
-                var total = PageViewPerformance.getDuration(timing.navigationStart, timing.loadEventEnd);
-                var network = PageViewPerformance.getDuration(timing.navigationStart, timing.connectEnd);
-                var request = PageViewPerformance.getDuration(timing.requestStart, timing.responseStart);
-                var response = PageViewPerformance.getDuration(timing.responseStart, timing.responseEnd);
-                var dom = PageViewPerformance.getDuration(timing.responseEnd, timing.loadEventEnd);
+
+            if (timings) {
+                durationMs = timings.duration;
+                if (timings.perfTotal) {
+                    this.perfTotal = timings.perfTotal;
+                    this.networkConnect = timings.networkConnect;
+                    this.sentRequest = timings.sentRequest;
+                    this.receivedResponse = timings.receivedResponse;
+                    this.domProcessing = timings.domProcessing;
+                }
+            } else {
+
+                var timing = PageViewPerformance.getPerformanceTiming();
+                if (timing) {
+                    var total = PageViewPerformance.getDuration(timing.navigationStart, timing.loadEventEnd);
+                    var network = PageViewPerformance.getDuration(timing.navigationStart, timing.connectEnd);
+                    var request = PageViewPerformance.getDuration(timing.requestStart, timing.responseStart);
+                    var response = PageViewPerformance.getDuration(timing.responseStart, timing.responseEnd);
+                    var dom = PageViewPerformance.getDuration(timing.responseEnd, timing.loadEventEnd);
 
 
-                if (total == 0) {
-                    _InternalLogging.throwInternalNonUserActionable(
-                        LoggingSeverity.WARNING,
-                        "error calculating page view performance: total='" +
-                        total + "', network='" + network + "', request='" + request + "', response='" +
-                        response + "', dom='" + dom + "'");
-                } else if (total < Math.floor(network) + Math.floor(request) + Math.floor(response) + Math.floor(dom)) {
-                    // some browsers may report individual components incorrectly so that the sum of the parts will be bigger than total PLT
-                    // in this case, don't report client performance from this page                    
-                    _InternalLogging.throwInternalNonUserActionable(
-                        LoggingSeverity.WARNING,
-                        "client performance math error:" + total + " < " + network + " + " + request + " + " + response + " + " + dom);
+                    if (total == 0) {
+                        _InternalLogging.throwInternalNonUserActionable(
+                            LoggingSeverity.WARNING,
+                            "error calculating page view performance: total='" +
+                            total + "', network='" + network + "', request='" + request + "', response='" +
+                            response + "', dom='" + dom + "'");
+                    } else if (total < Math.floor(network) + Math.floor(request) + Math.floor(response) + Math.floor(dom)) {
+                        // some browsers may report individual components incorrectly so that the sum of the parts will be bigger than total PLT
+                        // in this case, don't report client performance from this page                    
+                        _InternalLogging.throwInternalNonUserActionable(
+                            LoggingSeverity.WARNING,
+                            "client performance math error:" + total + " < " + network + " + " + request + " + " + response + " + " + dom);
 
-                } else {
+                    } else {
 
-                    // use timing data for duration if possible
-                    durationMs = total;
+                        // use timing data for duration if possible
+                        durationMs = total;
 
-                    // convert to timespans
-                    this.perfTotal = Util.msToTimeSpan(total);
-                    this.networkConnect = Util.msToTimeSpan(network);
-                    this.sentRequest = Util.msToTimeSpan(request);
-                    this.receivedResponse = Util.msToTimeSpan(response);
-                    this.domProcessing = Util.msToTimeSpan(dom);
+                        // convert to timespans
+                        this.perfTotal = Util.msToTimeSpan(total);
+                        this.networkConnect = Util.msToTimeSpan(network);
+                        this.sentRequest = Util.msToTimeSpan(request);
+                        this.receivedResponse = Util.msToTimeSpan(response);
+                        this.domProcessing = Util.msToTimeSpan(dom);
 
-                    this.isValid = true;
+                        this.isValid = true;
+                    }
                 }
             }
+
             this.url = Common.DataSanitizer.sanitizeUrl(url);
             this.name = Common.DataSanitizer.sanitizeString(name || Util.NotSpecified);
 
