f40e61a0fa8928548291cf36d30e49130d5e106a
mashekho@microsoft.com <mashekho@microsoft.com>
2015-11-19 13:25:25 -0800

    split ajax.ts

---
+++ JavaScript/JavaScriptSDK/ajax/ajaxRecord.ts
@@ -0,0 +1,76 @@
+﻿/// <reference path="../logging.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="./ajaxUtils.ts" />
+
+module Microsoft.ApplicationInsights {
+    "use strict";
+
+    export class ajaxRecord {
+        public async = false;
+        public completed = false;
+        public requestHeadersSize = null;
+        public ttfb = null;
+        public responseReceivingDuration = null;
+        public callbackDuration = null;
+        public ajaxTotalDuration = null;
+        public aborted = null;
+        public pageUrl = null;
+        public requestUrl = null;
+        public requestSize = 0;
+        public method = null;
+
+        ///<summary>Returns the HTTP status code.</summary>
+        public status = null;
+        public contentType = null;
+        public contentEncoding = null;
+        public responseSize = 0;
+
+        //<summary>The timestamp when open method was invoked</summary>
+        public requestSentTime = null;
+
+        //<summary>The timestamps when first byte was received</summary>
+        public responseStartedTime = null;
+
+        //<summary>The timestamp when last byte was received</summary>
+        public responseFinishedTime = null;
+
+        //<summary>The timestamp when onreadystatechange callback in readyState 4 finished</summary>
+        public callbackFinishedTime = null;
+
+        //<summary>True, if this request was performed when dom was loading, before document was interactive, otherwise false</summary>
+        public loadingRequest = false;
+
+        //<summary>The timestamp at which ajax was ended</summary>
+        public endTime = null;
+
+        //<summary>The original xhr onreadystatechange event</summary>
+        public originalOnreadystatechage = null;
+
+        //<summary>True, if onreadyStateChangeCallback function attached to xhr, otherwise false</summary>
+        public onreadystatechangeCallbackAttached = false;
+
+        //<summary>Determines whether or not JavaScript exception occured in xhr.onreadystatechange code. 1 if occured, otherwise 0.</summary>
+        public clientFailure = 0;
+
+        public getAbsoluteUrl = function () {
+            return this.requestUrl ? Util.getAbsoluteUrl(this.requestUrl) : null;
+        }
+
+        public CalculateMetrics = function () {
+            var self = this;
+            self.ttfb = dateTime.GetDuration(self.requestSentTime, self.responseStartedTime);
+            self.responseReceivingDuration = dateTime.GetDuration(self.responseStartedTime, self.responseFinishedTime);
+            self.callbackDuration = dateTime.GetDuration(self.responseFinishedTime, self.callbackFinishedTime);
+
+            var timeStamps = [self.responseStartedTime, self.responseFinishedTime, self.callbackFinishedTime];
+            for (var i = timeStamps.length - 1; i >= 0; i--) {
+                if (timeStamps[i] !== null) {
+                    self.endTime = timeStamps[i];
+                    self.ajaxTotalDuration = dateTime.GetDuration(self.requestSentTime, self.endTime);
+                    break;
+                }
+            }
+        };
+    };           
+
+}
---
+++ JavaScript/JavaScriptSDK/ajax/ajaxUtils.ts
@@ -0,0 +1,137 @@
+﻿/// <reference path="../logging.ts" />
+/// <reference path="../util.ts" />
+
+module Microsoft.ApplicationInsights {
+    "use strict";
+
+    export class constants {
+        public static attachEvent = "attachEvent";
+        public static de = "detachEvent";
+        public static ad = "addEventListener";
+        public static re = "removeEventListener";
+        public static udf = "undefined";
+    };
+
+    export class extensions {
+        public static IsNullOrUndefined(obj) {
+            return typeof (obj) === constants.udf || obj === null;
+        }
+    }
+
+    export class stringUtils {
+        public static GetLength(strObject) {
+            var res = 0;
+            if (!extensions.IsNullOrUndefined(strObject)) {
+                var stringified = "";
+                try {
+                    stringified = strObject.toString();
+                } catch (ex) {
+                    // some troubles with complex object
+                }
+
+                res = stringified.length;
+                res = isNaN(res) ? 0 : res;
+            }
+
+            return res;
+        }
+    }
+
+    export class dateTime {
+        ///<summary>Return the number of milliseconds since 1970/01/01 in local timezon</summary>
+        public static Now = function () {
+            return new Date().getTime();
+        }
+
+        ///<summary>Gets duration between two timestamps</summary>
+        public static GetDuration = function (start, end) {
+            var result = null;
+            if (start !== 0 && end !== 0 && !extensions.IsNullOrUndefined(start) && !extensions.IsNullOrUndefined(end)) {
+                result = end - start;
+            }
+
+            return result;
+        }
+    }
+
+    export class commands {
+
+        /// <summary>
+        /// Wrappes function call in try..catch block and trace exception in case it occurs
+        /// <param name="functionName">The name of the function which is wrapped</param>
+        /// <param name="funcPointer">Pointer to the function which needs to be wrappped</param>
+        /// <param name="params">Array of parameters that will be traced in case exception happends in the function</param>
+        /// </summary>
+        public static TryCatchTraceWrapper(functionName, funcPointer, params) {
+            try {
+                return funcPointer.call(this);
+            }
+            catch (ex) {
+                _InternalLogging.throwInternalNonUserActionable(
+                    LoggingSeverity.CRITICAL,
+                    "Failed calling callback '" + functionName + "': "
+                    + Microsoft.ApplicationInsights.Util.dump(ex));
+            }
+        }
+
+        ///<summary>Binds the specified function to an event, so that the function gets called whenever the event fires on the object</summary>
+        ///<param name="obj">Object to which </param>
+        ///<param name="eventNameWithoutOn">String that specifies any of the standard DHTML Events without "on" prefix</param>
+        ///<param name="handlerRef">Pointer that specifies the function to call when event fires</param>
+        ///<returns>True if the function was bound successfully to the event, otherwise false</returns>
+        public static AttachEvent(obj, eventNameWithoutOn, handlerRef) {
+            var result = false;
+            if (!extensions.IsNullOrUndefined(obj)) {
+                if (!extensions.IsNullOrUndefined(obj.attachEvent)) {
+
+                    // IE before version 9
+                    commands.TryCatchTraceWrapper(
+                        "attachEvent",
+                        function () {
+                            obj.attachEvent("on" + eventNameWithoutOn, handlerRef);
+                            result = true;
+                        },
+                        [obj, eventNameWithoutOn, constants.attachEvent]);
+                }
+                else {
+                    if (!extensions.IsNullOrUndefined(obj.addEventListener)) {
+
+                        // all browsers except IE before version 9
+                        commands.TryCatchTraceWrapper(
+                            "addEventListener",
+                            function () {
+                                obj.addEventListener(eventNameWithoutOn, handlerRef, false);
+                                result = true;
+                            },
+                            [obj, eventNameWithoutOn, constants.ad]);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static DetachEvent(obj, eventNameWithoutOn, handlerRef) {
+            if (!extensions.IsNullOrUndefined(obj)) {
+                if (!extensions.IsNullOrUndefined(obj.detachEvent)) {
+                    commands.TryCatchTraceWrapper(
+                        "detachEvent",
+                        function () {
+                            obj.detachEvent("on" + eventNameWithoutOn, handlerRef);
+                        },
+                        [obj.toString(), eventNameWithoutOn, constants.de]);
+                }
+                else {
+                    if (!extensions.IsNullOrUndefined(obj.removeEventListener)) {
+                        commands.TryCatchTraceWrapper(
+                            "removeEventListener",
+                            function () {
+                                obj.removeEventListener(eventNameWithoutOn, handlerRef, false);
+                            },
+                            [obj.toString(), eventNameWithoutOn, constants.re]);
+                    }
+                }
+            }
+        }
+    }
+}
