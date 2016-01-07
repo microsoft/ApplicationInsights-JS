385106ee40dd3dbd1626240e41ef44bbc2eb90d9
Max Shekhovtsov <Max.Shekhovtsov@microsoft.com>
2016-01-07 13:48:52 -0800

    Update AppInsights.ts

--- JavaScript/JavaScriptSDK/AppInsights.ts
+++ JavaScript/JavaScriptSDK/AppInsights.ts
@@ -12,7 +12,7 @@ module Microsoft.ApplicationInsights {
 
     "use strict";
 
-    export var Version = "0.21.0";
+    export var Version = "0.21.1";
 
     export interface IConfig {
         instrumentationKey: string;
