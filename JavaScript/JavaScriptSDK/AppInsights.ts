2b305e0bc4b46dd777ce0801398d7cc1c89b3da5
mashekho@microsoft.com <mashekho@microsoft.com>
2016-01-12 15:46:00 -0800

    bump up version

--- JavaScript/JavaScriptSDK/AppInsights.ts
+++ JavaScript/JavaScriptSDK/AppInsights.ts
@@ -12,7 +12,7 @@ module Microsoft.ApplicationInsights {
 
     "use strict";
 
-    export var Version = "0.21.2";
+    export var Version = "0.21.3";
 
     export interface IConfig {
         instrumentationKey: string;
