449018e05686e42eaebbde86c07d23513c5a8cd7
Max Shekhovtsov <Max.Shekhovtsov@microsoft.com>
2016-02-03 15:54:32 -0800

    Update ajax.ts
    Fixed return code - added >= 200 for success criteria

--- JavaScript/JavaScriptSDK/ajax/ajax.ts
+++ JavaScript/JavaScriptSDK/ajax/ajax.ts
@@ -197,7 +197,7 @@ module Microsoft.ApplicationInsights {
                     xhr.ajaxData.getAbsoluteUrl(),
                     xhr.ajaxData.getPathName(),
                     xhr.ajaxData.ajaxTotalDuration,
-                    (+(xhr.ajaxData.status)) < 400,
+                    (+(xhr.ajaxData.status)) >= 200 && (+(xhr.ajaxData.status)) < 400,
                     +xhr.ajaxData.status
                 );
 
