85e6156c83bd23f10cdca054c4a9d7e50ec100a6
Alex Bulankou <albulank@microsoft.com>
2016-02-09 14:30:42 -0800

    Incrementing version

--- JavaScript/JavaScriptSDK/AppInsights.ts
+++ JavaScript/JavaScriptSDK/AppInsights.ts
@@ -13,7 +13,7 @@ module Microsoft.ApplicationInsights {
 
     "use strict";
 
-    export var Version = "0.22.4";
+    export var Version = "0.22.5";
 
     export interface IConfig {
         instrumentationKey: string;
