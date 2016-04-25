755fe2558ed6a7880565ca28edb7373d8ab64b51
gregjhogan <gregjhogan@gmail.com>
2016-04-24 22:54:41 -0500

    SPA page view tracking

--- JavaScript/JavaScriptSDK/Initialization.ts
+++ JavaScript/JavaScriptSDK/Initialization.ts
@@ -156,6 +156,7 @@ module Microsoft.ApplicationInsights {
             config.emitLineDelimitedJson = Util.stringToBoolOrDefault(config.emitLineDelimitedJson);
             config.diagnosticLogInterval = config.diagnosticLogInterval || 10000;
             config.autoTrackPageVisitTime = ApplicationInsights.Util.stringToBoolOrDefault(config.autoTrackPageVisitTime);
+            config.singlePageApp = ApplicationInsights.Util.stringToBoolOrDefault(config.singlePageApp);
 
             if (isNaN(config.samplingPercentage) || config.samplingPercentage <= 0 || config.samplingPercentage >= 100) {
                 config.samplingPercentage = 100;
