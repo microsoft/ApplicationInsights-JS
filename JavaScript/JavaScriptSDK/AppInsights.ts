337031fb385109201f6c1f0abc76767fdd7c7764
Kamil Szostak <kszostak@microsoft.com>
2016-05-05 12:45:07 -0700

    Fix encoding

--- JavaScript/JavaScriptSDK/AppInsights.ts
+++ JavaScript/JavaScriptSDK/AppInsights.ts
@@ -1,4 +1,4 @@
-﻿/// <reference path="telemetrycontext.ts" />
+/// <reference path="telemetrycontext.ts" />
 /// <reference path="./Telemetry/Common/Data.ts"/>
 /// <reference path="./Util.ts"/>
 /// <reference path="./Contracts/Generated/SessionState.ts"/>
