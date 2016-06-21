9e95dfa962a5217fa6cc61a265229969c6f5d701
Kamil Szostak <kszostak@microsoft.com>
2016-06-21 16:09:59 -0700

    Add IEnvelope and ISerializable interfaces

---
+++ JavaScript/JavaScriptSDK.Interfaces/Telemetry/IEnvelope.ts
@@ -0,0 +1,22 @@
+﻿
+module Microsoft.ApplicationInsights {
+
+    "use strict";
+
+    export interface IEnvelope extends ISerializable {
+        ver: number;
+        name: string;
+        time: string;
+        sampleRate: number;
+        seq: string;
+        iKey: string;
+        flags: number;
+        deviceId: string;
+        os: string;
+        osVer: string;
+        appId: string;
+        appVer: string;
+        userId: string;
+        tags: { [name: string]: any };
+    }
+}
