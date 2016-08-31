b9d7bdfd143d247381a101b75048d2c2e8df7dc9
Kamil Szostak <kszostak@microsoft.com>
2016-08-31 14:35:22 -0700

    Move scripts to /scripts dir

--- scripts/runAllTests.cmd
+++ scripts/runAllTests.cmd
@@ -1,9 +1,9 @@
 @echo off
 @setlocal enabledelayedexpansion enableextensions
 
-set BuildRoot=%~dp0..\bin\debug\
+set BuildRoot=%~dp0..\..\bin\debug\
 
-for /F %%F in ('dir "..\bin\*tests*.dll" /s /b 2^> nul') do set TESTS=!TESTS! %%F
+for /F %%F in ('dir "..\..\bin\*tests*.dll" /s /b 2^> nul') do set TESTS=!TESTS! %%F
 set TESTS=%TESTS:~1%
 
 vstest.console /UseVsixExtensions:true %TESTS%
