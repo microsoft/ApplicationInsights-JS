93c9449eb177f92d65d5c6f5c00747d173b14f12
Kamil Szostak <kszostak@microsoft.com>
2017-05-01 17:46:50 -0700

    Grunt: compile typescript, create source maps

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    ts: {
+      default: {
+        src: [
+          '**/*.ts',
+          '!node_modules/**',
+          '!bundle/**',
+          '!dist/**',
+          '!JavaScript/JavaScriptSDK.Tests/**'],
+        out: 'bundle/ai.js',
+        comments: true
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'bundle/ai.min.js': ['bundle/ai.js']
+        },
+        options: {
+          sourceMap: true,
+          sourceMapIncludeSources: true,
+          sourceMapIn: 'bundle/ai.js.map'
+        },
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask("default", ["ts", "uglify"]);
+};
