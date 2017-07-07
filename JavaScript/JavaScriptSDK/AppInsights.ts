e718e8a425d91c5f7b8dfd8e314a6a46694f8842
Kamil Szostak <kszostak@microsoft.com>
2017-07-07 08:43:31 -0700

    version update 1.0.9 -> 1.0.10

--- JavaScript/JavaScriptSDK/AppInsights.ts
+++ JavaScript/JavaScriptSDK/AppInsights.ts
@@ -13,7 +13,7 @@ module Microsoft.ApplicationInsights {
 
     "use strict";
 
-    export var Version = "1.0.9";
+    export var Version = "1.0.10";
 
     /**
     * Internal interface to pass appInsights object to subcomponents without coupling 
