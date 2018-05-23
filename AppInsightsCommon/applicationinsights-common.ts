702fa70451767cd1ba368ccf6c966c7722ed1ebf
Basel Rustum <barustum@microsoft.com>
2018-05-23 15:27:44 -0700

    export more modules from common

--- AppInsightsCommon/applicationinsights-common.ts
+++ AppInsightsCommon/applicationinsights-common.ts
@@ -1,2 +1,5 @@
-export import CommonUtil = require("./Util");
-export {_InternalMessageId as _InternalMessageIdFoo} from './Enums'; 
+export { Util, CorrelationIdHelper, UrlHelper } from './Util';
+export { _InternalMessageId, LoggingSeverity } from './Enums';
+export { IConfig } from './Interfaces/IConfig';
+export { _InternalLogging, _InternalLogMessage } from './Logging';
+export { RequestHeaders } from './RequestResponseHeaders';
