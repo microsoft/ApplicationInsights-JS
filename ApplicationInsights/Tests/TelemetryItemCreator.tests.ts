f4fd590c9f538d3cbe7b6fbd7d30e3b04245daa1
Basel Rustum <barustum@microsoft.com>
2018-08-24 13:15:18 -0700

    add more unit tests

--- ApplicationInsights/Tests/TelemetryItemCreator.tests.ts
+++ ApplicationInsights/Tests/TelemetryItemCreator.tests.ts
@@ -1,7 +1,8 @@
 /// <reference path="./TestFramework/Common.ts" />
 
 import { TelemetryItemCreator } from "../JavaScriptSDK/TelemetryItemCreator";
-import { PageViewPerformance } from 'applicationinsights-common';
+import { PageViewPerformance, PageView } from 'applicationinsights-common';
+import { IPageViewTelemetry } from "../JavascriptSDK.Interfaces/IPageViewTelemetry";
 
 export class TelemetryItemCreatorTests extends TestClass {
     public registerTests() {
@@ -25,6 +26,38 @@ export class TelemetryItemCreatorTests extends TestClass {
 
                 // assert
                 Assert.ok(telemetryItem);
+                Assert.equal("Microsoft.ApplicationInsights.{0}.PageviewPerformance", telemetryItem.name, "telemtryItem.name");;
+                Assert.equal("PageviewPerformanceData", telemetryItem.baseType, "telemetryItem.baseType");
+                Assert.deepEqual({"propKey1":"PropVal1","propKey2":"PropVal2"},telemetryItem.data, "telemetryItem.data");
+            }
+        });
+
+        this.testCase({
+            name: "TelemetryItemCreatorTests: create a valid ITelemetryItem for a page view item",
+            test: () => {
+                // setup
+                let name = "testName";
+                let uri = "testUri";
+                var pageView: IPageViewTelemetry = {
+                    name: name,
+                    uri: uri
+                };
+                let properties = {
+                    "propKey1": "PropVal1",
+                    "propKey2": "PropVal2"
+                };
+
+                // act
+                let telemetryItem = TelemetryItemCreator.createItem(pageView,
+                    PageView.dataType,
+                    PageView.envelopeType,
+                    properties);
+
+                // assert
+                Assert.ok(telemetryItem);
+                Assert.equal("Microsoft.ApplicationInsights.{0}.Pageview", telemetryItem.name, "telemtryItem.name");;
+                Assert.equal("PageviewData", telemetryItem.baseType, "telemetryItem.baseType");
+                Assert.deepEqual({"propKey1":"PropVal1","propKey2":"PropVal2"},telemetryItem.data, "telemetryItem.data");
             }
         });
     }
