43bb6f8b8a3a216dd3ef8f1aacd34ec0aee18ce7
Mark Wolff <marwolff@microsoft.com>
2018-09-10 15:07:14 -0700

    Delete unused files

--- AppInsightsCommon/Interfaces/Context/ICoreContext.ts
+++
@@ -1,7 +0,0 @@
-import { IAppInsightsCore } from "applicationinsights-core-js";
-import IDiagnosticLogger from "applicationinsights-core-js/bundle/JavaScriptSDK.Interfaces/IDiagnosticLogger";
-
-export interface ICoreContext {
-    readonly core : IAppInsightsCore;
-    readonly logger: IDiagnosticLogger;
-}
