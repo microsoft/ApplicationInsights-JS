4a2af50818f5aebcbe1b265f6cc269a8d8e7b894
Mark Wolff <marwolff@microsoft.com>
2018-09-13 14:59:32 -0700

    Reorganize analytics tests

--- ApplicationInsights/Tests/ApplicationInsights.tests.ts
+++ ApplicationInsights/Tests/ApplicationInsights.tests.ts
@@ -40,7 +40,7 @@ export class ApplicationInsightsTests extends TestClass {
                 }
 
                 // act
-                var members = ["config", "trackException", "trackPageView"];
+                var members = ["config", "trackException", "_onerror", "trackPageView"];
                 while (members.length) {
                     leTest(members.pop());
                 }
@@ -48,6 +48,188 @@ export class ApplicationInsightsTests extends TestClass {
         });
 
         this.addStartStopTrackPageTests();
+        this.addTrackExceptionTests();
+        this.addOnErrorTests();
+    }
+
+    private addTrackExceptionTests(): void {
+        this.testCase({
+            name: "TrackExceptionTests: trackException accepts single exception and an array of exceptions",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                var appInsights = new ApplicationInsights();
+                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
+                let trackStub = this.sandbox.stub(appInsights.core, "track");
+
+                // Test
+                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Critical});
+                Assert.ok(trackStub.calledOnce, "single exception is tracked");
+                appInsights.trackException({error: [new Error()]});
+                Assert.ok(trackStub.calledTwice, "array of exceptions is tracked");
+            }
+        });
+
+        this.testCase({
+            name: "TrackExceptionTests: trackException allows logging errors with different severity level",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                var appInsights = new ApplicationInsights();
+                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
+                let trackStub = this.sandbox.stub(appInsights.core, "track");
+
+                // Test
+                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Critical});
+                Assert.ok(trackStub.calledOnce, "single exception is tracked");
+                Assert.equal(SeverityLevel.Critical, trackStub.firstCall.args[0].baseData.severityLevel);
+
+                trackStub.reset();
+
+                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Error});
+                Assert.ok(trackStub.calledOnce, "single exception is tracked");
+                Assert.equal(SeverityLevel.Error, trackStub.firstCall.args[0].baseData.severityLevel);
+            }
+        });
+    }
+
+    private addOnErrorTests(): void {
+        this.testCase({
+            name: "OnErrorTests: _onerror creates a dump of unexpected error thrown by trackException for logging",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                const appInsights = new ApplicationInsights();
+                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
+
+                const unexpectedError = new Error();
+                const dumpSpy = this.sandbox.stub(Util, "dump");
+                this.sandbox.stub(appInsights, "trackException").throws(unexpectedError);
+
+                // Act
+                appInsights._onerror({message: "msg", url: "some://url", lineNumber: 123, columnNumber: 456, error: unexpectedError});
+
+                // Assert
+                Assert.ok(dumpSpy.calledWith(unexpectedError))
+            }
+        });
+
+        this.testCase({
+            name: "OnErrorTests: _onerror stringifies error object",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                var appInsights = new ApplicationInsights();
+                appInsights.initialize({ instrumentationKey: "ikey"}, core, []);
+                var dumpSpy = this.sandbox.spy(Util, "dump")
+                var unexpectedError = new Error("some message");
+                var stub = this.sandbox.stub(appInsights, "trackException").throws(unexpectedError);
+
+                // Act
+                appInsights._onerror({message: "any message", url: "any://url", lineNumber: 123, columnNumber: 456, error: unexpectedError});
+
+                // Test
+                Assert.ok(dumpSpy.returnValues[0].indexOf("stack: ") != -1);
+                Assert.ok(dumpSpy.returnValues[0].indexOf(`message: '${unexpectedError.message}'`) != -1);
+                Assert.ok(dumpSpy.returnValues[0].indexOf("name: 'Error'") != -1);
+            }
+        });
+
+        this.testCase({
+            name: "OnErrorTests: _onerror logs name of unexpected error thrown by trackException for diagnostics",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                const core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key",
+                    enableExceptionAutoCollection: true},
+                    [plugin]
+                );
+                const appInsights = new ApplicationInsights();
+                appInsights.initialize({ instrumentationKey: "key", enableExceptionAutoCollection: true }, core, []);
+
+                const throwInternal = this.sandbox.spy(appInsights.core.logger, "throwInternal");
+                const nameStub = this.sandbox.stub(Util, "getExceptionName");
+
+                this.sandbox.stub(appInsights, "trackException").throws(new Error());
+                const expectedErrorName: string = "test error";
+
+                nameStub.returns(expectedErrorName);
+
+                appInsights._onerror({message: "some message", url: "some://url", lineNumber: 1234, columnNumber: 5678, error: new Error()});
+
+                Assert.ok(throwInternal.calledOnce, "throwInternal called once");
+                const logMessage: string = throwInternal.getCall(0).args[2];
+                Assert.notEqual(-1, logMessage.indexOf(expectedErrorName));
+            }
+        });
+
+        this.testCase({
+            name: "OnErrorTests: _onerror adds document URL in case of CORS error",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                const appInsights = new ApplicationInsights();
+                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
+                const trackSpy = this.sandbox.spy(appInsights.core, "track");
+
+                // Act
+                appInsights._onerror({message: "Script error.", url: "", lineNumber: 0, columnNumber: 0, error: null});
+
+                // Assert
+                Assert.equal(document.URL, trackSpy.args[0][0].baseData.url);
+            }
+        });
+
+        this.testCase({
+            name: "OnErrorTests: _onerror adds document URL in case of no CORS error",
+            test: () => {
+                // setup
+                const plugin = new TestPlugin();
+                var core = new AppInsightsCore();
+                core.initialize(
+                    {instrumentationKey: "key"},
+                    [plugin]
+                );
+                var appInsights = new ApplicationInsights();
+                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
+                const trackExceptionSpy = this.sandbox.spy(appInsights, "trackException");
+
+                // Act
+                // Last arg is not an error/null which will be treated as not CORS issue
+                appInsights._onerror({message: "Script error.", url: "", lineNumber: 0, columnNumber: 0, error: <any>new Object()});
+
+                // Assert
+                // properties are passed as a 3rd parameter
+                Assert.equal(document.URL, trackExceptionSpy.args[0][1].url);
+            }
+        });
     }
 
     private addStartStopTrackPageTests() {
@@ -207,183 +389,6 @@ export class ApplicationInsightsTests extends TestClass {
                     Assert.ok(logStub.calledOnce, "calling stop without a corresponding start triggers warning to user");
                 }
         });
-
-        this.testCase({
-            name: "AppInsightsTests: trackException accepts single exception and an array of exceptions",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                var appInsights = new ApplicationInsights();
-                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
-                let trackStub = this.sandbox.stub(appInsights.core, "track");
-
-                // Test
-                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Critical});
-                Assert.ok(trackStub.calledOnce, "single exception is tracked");
-                appInsights.trackException({error: [new Error()]});
-                Assert.ok(trackStub.calledTwice, "array of exceptions is tracked");
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: trackException allows logging errors with different severity level",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                var appInsights = new ApplicationInsights();
-                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
-                let trackStub = this.sandbox.stub(appInsights.core, "track");
-
-                // Test
-                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Critical});
-                Assert.ok(trackStub.calledOnce, "single exception is tracked");
-                Assert.equal(SeverityLevel.Critical, trackStub.firstCall.args[0].baseData.severityLevel);
-
-                trackStub.reset();
-
-                appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Error});
-                Assert.ok(trackStub.calledOnce, "single exception is tracked");
-                Assert.equal(SeverityLevel.Error, trackStub.firstCall.args[0].baseData.severityLevel);
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: _onerror creates a dump of unexpected error thrown by trackException for logging",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                const appInsights = new ApplicationInsights();
-                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
-
-                const unexpectedError = new Error();
-                const dumpSpy = this.sandbox.stub(Util, "dump");
-                this.sandbox.stub(appInsights, "trackException").throws(unexpectedError);
-
-                // Act
-                appInsights._onerror({message: "msg", url: "some://url", lineNumber: 123, columnNumber: 456, error: unexpectedError});
-
-                // Assert
-                Assert.ok(dumpSpy.calledWith(unexpectedError))
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: _onerror stringifies error object",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                var appInsights = new ApplicationInsights();
-                appInsights.initialize({ instrumentationKey: "ikey"}, core, []);
-                var dumpSpy = this.sandbox.spy(Util, "dump")
-                var unexpectedError = new Error("some message");
-                var stub = this.sandbox.stub(appInsights, "trackException").throws(unexpectedError);
-
-                // Act
-                appInsights._onerror({message: "any message", url: "any://url", lineNumber: 123, columnNumber: 456, error: unexpectedError});
-
-                // Test
-                Assert.ok(dumpSpy.returnValues[0].indexOf("stack: ") != -1);
-                Assert.ok(dumpSpy.returnValues[0].indexOf(`message: '${unexpectedError.message}'`) != -1);
-                Assert.ok(dumpSpy.returnValues[0].indexOf("name: 'Error'") != -1);
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: _onerror logs name of unexpected error thrown by trackException for diagnostics",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                const core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key",
-                    enableExceptionAutoCollection: true},
-                    [plugin]
-                );
-                const appInsights = new ApplicationInsights();
-                appInsights.initialize({ instrumentationKey: "key", enableExceptionAutoCollection: true }, core, []);
-
-                const throwInternal = this.sandbox.spy(appInsights.core.logger, "throwInternal");
-                const nameStub = this.sandbox.stub(Util, "getExceptionName");
-
-                this.sandbox.stub(appInsights, "trackException").throws(new Error());
-                const expectedErrorName: string = "test error";
-
-                nameStub.returns(expectedErrorName);
-
-                appInsights._onerror({message: "some message", url: "some://url", lineNumber: 1234, columnNumber: 5678, error: new Error()});
-
-                Assert.ok(throwInternal.calledOnce, "throwInternal called once");
-                const logMessage: string = throwInternal.getCall(0).args[2];
-                Assert.notEqual(-1, logMessage.indexOf(expectedErrorName));
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: _onerror adds document URL in case of CORS error",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                const appInsights = new ApplicationInsights();
-                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
-                const trackSpy = this.sandbox.spy(appInsights.core, "track");
-
-                // Act
-                appInsights._onerror({message: "Script error.", url: "", lineNumber: 0, columnNumber: 0, error: null});
-
-                // Assert
-                Assert.equal(document.URL, trackSpy.args[0][0].baseData.url);
-            }
-        });
-
-        this.testCase({
-            name: "AppInsightsTests: _onerror adds document URL in case of no CORS error",
-            test: () => {
-                // setup
-                const plugin = new TestPlugin();
-                var core = new AppInsightsCore();
-                core.initialize(
-                    {instrumentationKey: "key"},
-                    [plugin]
-                );
-                var appInsights = new ApplicationInsights();
-                appInsights.initialize({ "instrumentationKey": "ikey" }, core, []);
-                const trackExceptionSpy = this.sandbox.spy(appInsights, "trackException");
-
-                // Act
-                // Last arg is not an error/null which will be treated as not CORS issue
-                appInsights._onerror({message: "Script error.", url: "", lineNumber: 0, columnNumber: 0, error: <any>new Object()});
-
-                // Assert
-                // properties are passed as a 3rd parameter
-                Assert.equal(document.URL, trackExceptionSpy.args[0][1].url);
-            }
-        });
-
     }
 }
 
