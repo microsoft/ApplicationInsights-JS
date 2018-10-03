77d679a273561c0a568440c4811f0bd2e7b8052e
Mark Wolff <marwolff@microsoft.com>
2018-10-03 14:34:10 -0700

    publish 0.0.15

--- AISKU/snippet/snippet.js
+++ AISKU/snippet/snippet.js
@@ -11,7 +11,7 @@ var aisdk = window[aiName] || (function (aiConfig) {
     var localDocument = document, localWindow = window, scriptText = "script";
     setTimeout(function () {
         var scriptElement = localDocument.createElement(scriptText);
-        scriptElement.src = aiConfig.url || "https://1dsjssdk.blob.core.windows.net/scripts/aisdk.0.0.14-beta.min.js";
+        scriptElement.src = aiConfig.url || "https://1dsjssdk.blob.core.windows.net/scripts/aisdk.0.0.15.min.js";
         localDocument.getElementsByTagName(scriptText)[0].parentNode.appendChild(scriptElement);
     });
 
