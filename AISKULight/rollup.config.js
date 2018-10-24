c5049d759335c0a30a3600e27f7d9f84c95c9601
Mark Wolff <marwolff@microsoft.com>
2018-10-24 16:14:19 -0700

    update aiskulite for ESM changes

--- AISKULight/rollup.config.js
+++ AISKULight/rollup.config.js
@@ -1,14 +1,52 @@
-import resolve from 'rollup-plugin-node-resolve';
+import nodeResolve from "rollup-plugin-node-resolve";
+import visualizer from "rollup-plugin-visualizer";
+import {uglify} from "rollup-plugin-uglify";
 
-export default [
-  {
-    input: "bundle/index.js",
+const version = require("./package.json").version;
+const banner = [
+  "/*!",
+  ` * Application Insights JavaScript SDK - Common, ${version}`,
+  " * Copyright (c) Microsoft and contributors. All rights reserved.",
+  " */"
+].join("\n");
+
+const browserRollupConfigFactory = isProduction => {
+  const browserRollupConfig = {
+    input: "dist-esm/index.js",
     output: {
-      file: "dist/aisdklight.js",
+      file: "browser/index.js",
+      banner: banner,
       format: "umd",
-      name: "aisdklite",
+      name: "aicommon",
       sourcemap: true
     },
-    plugins: [resolve()]
+    plugins: [
+      nodeResolve({
+        browser: false,
+        preferBuiltins: false
+      })
+    ]
+  };
+
+  if (isProduction) {
+    browserRollupConfig.output.file = "browser/index.min.js";
+    browserRollupConfig.plugins.push(
+      uglify({
+        output: {
+          preamble: banner
+        }
+      }),
+      visualizer({
+        filename: "./statistics.html",
+        sourcemap: true
+      })
+    );
   }
+
+  return browserRollupConfig;
+};
+
+export default [
+  browserRollupConfigFactory(true),
+  browserRollupConfigFactory(false)
 ];
