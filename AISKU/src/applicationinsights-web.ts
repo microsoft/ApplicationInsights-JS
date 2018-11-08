0b264acc6e322a93be01ad8ef77b408223c4e621
Mark Wolff <marwolff@microsoft.com>
2018-11-08 13:43:10 -0800

    Remove browser field from package.json, export types in main

--- AISKU/src/applicationinsights-web.ts
+++ AISKU/src/applicationinsights-web.ts
@@ -3,3 +3,11 @@ export {
     IApplicationInsights,
     Snippet
 } from './Initialization';
+
+
+export { IConfiguration, AppInsightsCore, IAppInsightsCore, LoggingSeverity, _InternalMessageId, ITelemetryItem } from "@microsoft/applicationinsights-core-js";
+export { ApplicationInsights as ApplicationAnalytics } from "@microsoft/applicationinsights-analytics-js";
+export { Util, IConfig, IDependencyTelemetry, PageViewPerformance, IPageViewPerformanceTelemetry, IPageViewTelemetry, IExceptionTelemetry, IAutoExceptionTelemetry, ITraceTelemetry, IMetricTelemetry, IEventTelemetry, IAppInsights } from "@microsoft/applicationinsights-common";
+export { Sender } from "@microsoft/applicationinsights-channel-js";
+export { PropertiesPlugin, IPropertiesPlugin } from "@microsoft/applicationinsights-properties-js";
+export { AjaxPlugin as DependenciesPlugin, IDependenciesPlugin } from '@microsoft/applicationinsights-dependencies-js';
