091576592cc47d2fb1a1c1149d61af00663104c3
Mark Wolff <marwolff@microsoft.com>
2018-11-12 13:01:00 -0800

    Change fallback global scope to Microsoft

--- AISKU/rollup.config.js
+++ AISKU/rollup.config.js
@@ -17,7 +17,7 @@ const browserRollupConfigFactory = (isProduction, libVersion = '1') => {
       file: `browser/ai.${libVersion}.js`,
       banner: banner,
       format: "umd",
-      name: "aisdk",
+      name: "Microsoft",
       sourcemap: true
     },
     plugins: [
@@ -56,7 +56,7 @@ const nodeUmdRollupConfigFactory = (isProduction) => {
       file: `dist/applicationinsights-web.js`,
       banner: banner,
       format: "umd",
-      name: "ai",
+      name: "Microsoft",
       sourcemap: true
     },
     plugins: [
