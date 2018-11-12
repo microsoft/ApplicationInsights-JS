55715171809e5c3ad5d072da6e232273ab246c96
Mark Wolff <Mark.Wolff@microsoft.com>
2018-11-12 14:38:46 -0800

    Add window webworker checks to Common (Basic SKU) (#712)
    * Add window webworker checks to Util
    
    * fix window webworker check
    
    * Reexport dependency types in both SKU
    
    * Change fallback global scope to Microsoft

--- AISKU/src/applicationinsights-web.ts
+++ AISKU/src/applicationinsights-web.ts
@@ -1,13 +1,48 @@
-export { 
+export {
     Initialization as ApplicationInsights,
     IApplicationInsights,
     Snippet
-} from './Initialization';
+} from "./Initialization";
 
-
-export { IConfiguration, AppInsightsCore, IAppInsightsCore, LoggingSeverity, _InternalMessageId, ITelemetryItem } from "@microsoft/applicationinsights-core-js";
-export { ApplicationInsights as ApplicationAnalytics } from "@microsoft/applicationinsights-analytics-js";
-export { Util, IConfig, IDependencyTelemetry, PageViewPerformance, IPageViewPerformanceTelemetry, IPageViewTelemetry, IExceptionTelemetry, IAutoExceptionTelemetry, ITraceTelemetry, IMetricTelemetry, IEventTelemetry, IAppInsights } from "@microsoft/applicationinsights-common";
+// Re-exports
+export {
+    IConfiguration,
+    AppInsightsCore,
+    IAppInsightsCore,
+    LoggingSeverity,
+    _InternalMessageId,
+    ITelemetryItem
+} from "@microsoft/applicationinsights-core-js";
+export {
+    Util,
+    IConfig,
+    IDependencyTelemetry,
+    IPageViewPerformanceTelemetry,
+    IPageViewTelemetry,
+    IExceptionTelemetry,
+    IAutoExceptionTelemetry,
+    ITraceTelemetry,
+    IMetricTelemetry,
+    IEventTelemetry,
+    IAppInsights,
+    SeverityLevel,
+    Event,
+    Exception,
+    Metric,
+    PageView,
+    PageViewPerformance,
+    RemoteDependencyData,
+    Trace
+} from "@microsoft/applicationinsights-common";
 export { Sender } from "@microsoft/applicationinsights-channel-js";
-export { PropertiesPlugin, IPropertiesPlugin } from "@microsoft/applicationinsights-properties-js";
-export { AjaxPlugin as DependenciesPlugin, IDependenciesPlugin } from '@microsoft/applicationinsights-dependencies-js';
+export {
+    ApplicationInsights as ApplicationAnalytics
+} from "@microsoft/applicationinsights-analytics-js";
+export {
+    PropertiesPlugin,
+    IPropertiesPlugin
+} from "@microsoft/applicationinsights-properties-js";
+export {
+    AjaxPlugin as DependenciesPlugin,
+    IDependenciesPlugin
+} from "@microsoft/applicationinsights-dependencies-js";
