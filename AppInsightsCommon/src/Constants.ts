71a4fdec09459eb895cf695e05b6ac1776bb8a23
Piyali Jana <piyalij@microsoft.com>
2018-12-17 04:11:55 -0800

    Changes in common to map dependency telemetry to 1ds docs, add constants for globals, export global var for ContextTagKeys, add partA transform method between old and CS4 part a definition

--- AppInsightsCommon/src/Constants.ts
+++ AppInsightsCommon/src/Constants.ts
@@ -2,3 +2,6 @@
 // Licensed under the MIT License.
 
 export const DisabledPropertyName: string = "Microsoft_ApplicationInsights_BypassAjaxInstrumentation";
+export const SampleRate = "sampleRate";
+export const ProcessLegacy = "ProcessLegacy";
+export const HttpMethod = "http.method";
