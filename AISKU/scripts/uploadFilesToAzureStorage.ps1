61db6369dcf0ee0744a0d9403ddecd0b89d378d4
Mark Wolff <marwolff@microsoft.com>
2019-01-30 13:23:13 -0800

    Add next, beta, scripts menu

--- AISKU/scripts/uploadFilesToAzureStorage.ps1
+++ AISKU/scripts/uploadFilesToAzureStorage.ps1
@@ -1,5 +1,36 @@
 $storageAccountName = "daprodcdn";
-$containerName = "next";
+$cacheControlValue = "max-age=604800, immutable";
+$contentType = "text/javascript; charset=utf-8";
+
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Select container name'
+     )
+     cls
+     Write-Host "================ $Title ================"
+
+     Write-Host "1: next"
+     Write-Host "2: beta"
+     Write-Host "3: scripts/b"
+}
+Show-Menu
+$input = Read-Host "Select an option (1, 2, 3)"
+
+switch ($input)
+{
+    '1' {
+        $containerName = "next";
+    } '2' {
+        $containerName = "beta";
+    } '3' {
+        $containerName = "scripts";
+    }
+}
+
+# Add "b/" blob prefix for scripts container only
+$blobPrefix = If ($containerName.equals("scripts")) {"b/"} Else {""}
+
 
 # get js sdk directory
 $jsSdkDir = Split-Path (Split-Path $MyInvocation.MyCommand.Path) -Parent;
@@ -50,13 +81,11 @@ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 $storageAccountKey = Read-Host "Please enter '$storageAccountName' account key";
 
 $azureContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
-$cacheControlValue = "max-age=604800, immutable";
-$contentType = "text/javascript; charset=utf-8";
 
 # upload files to Azure Storage
 $files = Get-ChildItem $releaseFromDir;
 foreach($file in $files) {
-    Set-AzureStorageBlobContent -Container $containerName -File (Join-Path $releaseFromDir -ChildPath $file) -Blob ($file) -Context $azureContext -Properties @{CacheControl = $cacheControlValue; ContentType = $contentType}
+    Set-AzureStorageBlobContent -Container $containerName -File (Join-Path $releaseFromDir -ChildPath $file) -Blob ($blobPrefix + $file) -Context $azureContext -Properties @{CacheControl = $cacheControlValue; ContentType = $contentType}
 }
 
 Write-Host "Files uploaded successfully to Azure Storage."
