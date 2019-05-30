b5342914765685f53ac8ddae4f0cb77d4a04b288
Mark Wolff <marwolff@microsoft.com>
2019-05-30 15:03:29 -0700

    rollup: revert delete browser config

--- vNext/extensions/applicationinsights-react-js/rollup.config.js
+++ vNext/extensions/applicationinsights-react-js/rollup.config.js
@@ -12,6 +12,45 @@ const banner = [
   " */"
 ].join("\n");
 
+const browserRollupConfigFactory = isProduction => {
+  const browserRollupConfig = {
+    input: `dist-esm/${outputName}.js`,
+    output: {
+      file: `browser/${outputName}.js`,
+      banner: banner,
+      format: "umd",
+      name: "Microsoft.ApplicationInsights",
+      sourcemap: true
+    },
+    plugins: [
+      replace({
+        delimiters: ["", ""],
+        values: {
+          "// Copyright (c) Microsoft Corporation. All rights reserved.": "",
+          "// Licensed under the MIT License.": ""
+        }
+      }),
+      nodeResolve({
+        browser: false,
+        preferBuiltins: false
+      })
+    ]
+  };
+
+   if (isProduction) {
+    browserRollupConfig.output.file = `browser/${outputName}.min.js`;
+    browserRollupConfig.plugins.push(
+      uglify({
+        output: {
+          preamble: banner
+        }
+      })
+    );
+  }
+
+  return browserRollupConfig;
+};
+
 const nodeUmdRollupConfigFactory = (isProduction) => {
   const nodeRollupConfig = {
     input: `dist-esm/${outputName}.js`,
@@ -55,6 +94,8 @@ const nodeUmdRollupConfigFactory = (isProduction) => {
 };
 
 export default [
-  nodeUmdRollupConfigFactory(false),
+  browserRollupConfigFactory(true),
+  browserRollupConfigFactory(false),
+  nodeUmdRollupConfigFactory(true),
   nodeUmdRollupConfigFactory(false)
 ];
