62265146e4df28a825035ce37119b4319488ef1d
Mark Wolff <marwolff@microsoft.com>
2019-08-14 13:41:04 -0700

    bump versions to 2.2.0 (#1001)

--- vNext/extensions/applicationinsights-properties-js/src/Context/Internal.ts
+++ vNext/extensions/applicationinsights-properties-js/src/Context/Internal.ts
@@ -4,7 +4,7 @@
 import { IInternal } from '@microsoft/applicationinsights-common';
 import { ITelemetryConfig } from '../Interfaces/ITelemetryConfig';
 
-const Version = "2.1.1";
+const Version = "2.2.0";
 
 export class Internal implements IInternal {
 
