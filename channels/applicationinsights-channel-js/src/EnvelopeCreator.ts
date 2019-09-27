7e4d80d5abf597722e2a242976e451654473fb6a
Xiao Li <lxiao@microsoft.com>
2019-09-26 19:23:40 -0700

    fix lint

--- channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
+++ channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
@@ -58,7 +58,9 @@ export abstract class EnvelopeCreator {
     // TODO: Do we want this to take logger as arg or use this._logger as nonstatic?
     protected static createEnvelope<T>(logger: IDiagnosticLogger, envelopeType: string, telemetryItem: ITelemetryItem, data: Data<T>): IEnvelope {
         const envelope = new Envelope(logger, data, envelopeType);
-        if (telemetryItem[SampleRate]) envelope.sampleRate = telemetryItem[SampleRate];
+        if (telemetryItem[SampleRate]) {
+            envelope.sampleRate = telemetryItem[SampleRate];
+        }
         envelope.iKey = telemetryItem.iKey;
         const iKeyNoDashes = telemetryItem.iKey.replace(/-/g, "");
         envelope.name = envelope.name.replace("{0}", iKeyNoDashes);
