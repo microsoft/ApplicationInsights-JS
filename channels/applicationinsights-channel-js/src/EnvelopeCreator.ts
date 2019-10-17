b3d82fe45cdb281abecef92a2313dd79ed977bd7
Mark Wolff <marwolff@microsoft.com>
2019-10-17 14:53:57 -0700

    chore: bump version to 2.3.0 (#1092)
    * chore: bump version to 2.3.0
    
    * fix: wrong version bump

--- channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
+++ channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
@@ -19,7 +19,7 @@ const baseType: string = "baseType";
 const baseData: string = "baseData";
 
 export abstract class EnvelopeCreator {
-    public static Version = "2.2.4";
+    public static Version = "2.3.0";
 
     protected static extractPropsAndMeasurements(data: { [key: string]: any }, properties: { [key: string]: any }, measurements: { [key: string]: any }) {
         if (!CoreUtils.isNullOrUndefined(data)) {
