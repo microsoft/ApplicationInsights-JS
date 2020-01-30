50387d9b49f90fa2fb8ac47a1288d38a9246d5ba
Nev Wylie <newylie@microsoft.com>
2020-01-29 18:38:57 -0800

    Bump Version to 2.4.1 and add Releases doc

--- channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
+++ channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
@@ -20,7 +20,7 @@ const baseType: string = "baseType";
 const baseData: string = "baseData";
 
 export abstract class EnvelopeCreator {
-    public static Version = "2.4.0";
+    public static Version = "2.4.1";
 
     protected static extractPropsAndMeasurements(data: { [key: string]: any }, properties: { [key: string]: any }, measurements: { [key: string]: any }) {
         if (!CoreUtils.isNullOrUndefined(data)) {
