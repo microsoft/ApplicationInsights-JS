d99651d3f6a5b120be6cd3ce47282454998d1208
xiao-lix <lixiao900921@gmail.com>
2021-01-12 16:53:51 -0800

    update angular plugin

--- extensions/applicationinsights-angularplugin-js/src/applicationinsights-angularplugin-js.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import '@microsoft/applicationinsights-shims';
-import AngularPlugin from "./AngularPlugin";
-import { AngularPluginService } from "./AngularPluginService";
-import { IAngularExtensionConfig } from "./Interfaces/IAngularExtensionConfig";
-
-export { AngularPlugin, AngularPluginService, IAngularExtensionConfig };
