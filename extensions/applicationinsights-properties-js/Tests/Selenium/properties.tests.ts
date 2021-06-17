f966bf7599620a0b92389f4e633b2119cc1694ff
xiao-lix <lixiao900921@gmail.com>
2021-06-17 12:48:20 -0700

    ai_user cookie should use name prefix for cookie storage when nameprefix is configured

--- extensions/applicationinsights-properties-js/Tests/Selenium/properties.tests.ts
+++ extensions/applicationinsights-properties-js/Tests/Selenium/properties.tests.ts
@@ -4,6 +4,7 @@ import PropertiesPlugin from "../../src/PropertiesPlugin";
 import { ITelemetryConfig } from "../../src/Interfaces/ITelemetryConfig";
 import { TelemetryContext } from "../../src/TelemetryContext";
 import { TelemetryTrace } from "../../src/Context/TelemetryTrace";
+import { IConfig } from "@microsoft/applicationinsights-common";
 
 export class PropertiesTests extends AITestClass {
     private properties: PropertiesPlugin;
@@ -193,6 +194,30 @@ export class PropertiesTests extends AITestClass {
                 Assert.equal(true, expiration.substr(0, "expires=".length) === "expires=", "ai_user cookie expiration part should start with expires=");
                 var expirationDate = new Date(expiration.substr("expires=".length));
                 Assert.equal(true, expirationDate > (new Date), "ai_user cookie expiration should be in the future");
+            }
+        });
+
+        this.testCase({
+            name: "ai_user cookie uses name prefix for cookie storage",
+            test: () => {
+                // setup
+                var actualCookieName: string;
+                var actualCookieValue: string;
+
+                var newIdStub = this.sandbox.stub(this as any, "_getNewId").callsFake(() => "newId");
+                var getCookieStub = this.sandbox.stub(this as any, "_getCookie").callsFake(() =>"");
+                var setCookieStub = this.sandbox.stub(this as any, "_setCookie").callsFake((cookieName, cookieValue) => {
+                    actualCookieName = cookieName;
+                    actualCookieValue = cookieValue;
+                });
+
+                // act
+                let config: IConfig & IConfiguration = this.getEmptyConfig();
+                config.namePrefix = 'testNamePrefix';
+                this.properties.initialize(config, this.core, []);
+
+                // verify
+                Assert.equal("ai_usertestNamePrefix", actualCookieName, "ai_user cookie is set");
             }
         });
 
