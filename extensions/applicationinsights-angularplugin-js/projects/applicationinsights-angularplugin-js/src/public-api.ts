16a4284fa410cb0a54a4db5e093ddd83af626eec
Nev <54870357+MSNev@users.noreply.github.com>
2021-07-09 10:47:33 -0700

    Remove AngularPlugin code from master and direct to new repo and angularplugin-legacy branch (#1607)

--- extensions/applicationinsights-angularplugin-js/projects/applicationinsights-angularplugin-js/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of applicationinsights-angularplugin-js
- */
-
-export * from './lib/applicationinsights-angularplugin-js.component';
-export * from './lib/applicationinsights-angularplugin-js.module';
