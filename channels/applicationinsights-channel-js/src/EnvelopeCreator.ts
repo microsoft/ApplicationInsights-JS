c8e40fffd5079be3f512005e4dd580c3144fc62a
Nev <54870357+MSNev@users.noreply.github.com>
2022-02-28 13:10:08 -0800

    [Release] Increase version to 2.7.4 (#1770)
    - Updates React Plugin to v3.2.4 (with v2.7.4 as dependency)
    - Updates React Native Plugin to 2.4.4 (with v2.7.4 as dependency)
    - Updates Chrome Debug Extension to 0.2.4
    
    This release is primarily a performance improvement release where we will now use any built in (or provided polyfill) function
    over the internal polyfills for
    
    - String trim()
    - String endsWith()
    - String startsWith()
    - Additional Date toISOString()
    - Array isArray()
    - Array indexOf()
    - Array map()
    - Array reduce()
    - Object freeze()
    - Object seal()

--- channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
+++ channels/applicationinsights-channel-js/src/EnvelopeCreator.ts
@@ -175,7 +175,7 @@ function EnvelopeCreatorInit(logger: IDiagnosticLogger, telemetryItem: ITelemetr
 }
 
 export const EnvelopeCreator = {
-    Version: "2.7.3"
+    Version: "2.7.4"
 };
 
 export function DependencyEnvelopeCreator(logger: IDiagnosticLogger, telemetryItem: ITelemetryItem, customUndefinedValue?: any): IEnvelope {
