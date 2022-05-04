55cdecbb19e4e81d504e12257c230bf5fc51c2da
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2022-05-04 13:45:55 -0700

    [Release] Increase version to 2.7.6 - Updates to dynamicProto v1.1.6 and add manual test page   - [IE8] Fix in 1.1.5 only handles 2 levels of dynamically nested classes #50

--- extensions/applicationinsights-clickanalytics-js/src/ClickAnalyticsPlugin.ts
+++ extensions/applicationinsights-clickanalytics-js/src/ClickAnalyticsPlugin.ts
@@ -27,7 +27,7 @@ export { BehaviorMapValidator, BehaviorValueValidator, BehaviorEnumValidator }
 export class ClickAnalyticsPlugin extends BaseTelemetryPlugin {
     public identifier: string = "ClickAnalyticsPlugin";
     public priority: number = 181;
-    public static Version = "2.7.5";
+    public static Version = "2.7.6";
     private _config: IClickAnalyticsConfiguration;
     private pageAction: PageAction;
     private _autoCaptureHandler: IAutoCaptureHandler;
