436191594d37ec7d6900e55fae46194dcb0b4601
Nev <54870357+MSNev@users.noreply.github.com>
2023-04-05 10:14:01 -0700

    Update NPM publish scripts (#2032)

--- tools/release-tools/npm_publish.js
+++ tools/release-tools/npm_publish.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const child_process = require("child_process");
 
-let packageRoot;
+const packageGroupDef = "./tools/release-tools/package_groups.json";
+let packageGroup;
 let dryRun = "";
 
 function showHelp() {
@@ -16,9 +17,9 @@ function showHelp() {
     }
 
     console.log("");
-    console.log(scriptName + " <pkgFolder> ");
+    console.log(scriptName + " <group> ");
     console.log("--------------------------");
-    console.log(" <pkgFolder>      - Identifies the folder containing the package.json and *.tgz");
+    console.log(" <group>      - Identifies the group to publish, identifies folders");
 }
 
 function parseArgs() {
@@ -27,15 +28,18 @@ function parseArgs() {
         return false;
     }
 
-    console.log("cwd: " + process.cwd());
-
     let idx = 2;
     while (idx < process.argv.length) {
         let theArg = process.argv[idx];
-        if (!packageRoot) {
-            packageRoot = theArg;
-        } else if(theArg === "-test") {
-            dryRun = "--dry-run";
+        if (theArg.startsWith("-")) {
+            if (theArg === "-test") {
+                dryRun = "--dry-run";
+            } else {
+                console.error("!!! Unknown switch [" + theArg + "] detected");
+                return false;
+            }
+        } else if (!packageGroup) {
+            packageGroup = theArg;
         } else {
             console.error("!!! Invalid Argument [" + theArg + "] detected");
             return false;
@@ -51,6 +55,10 @@ function removeTrailingComma(text) {
     return text.replace(/,(\s*[}\],])/g, "$1");
 }
 
+function removeComments(text) {
+    return text.replace(/^\s*\/\/\s.*$/gm, "");
+}
+
 function getNpmPackageName(packageJsonFile) {
     var packageText = removeTrailingComma(fs.readFileSync(packageJsonFile, "utf-8"));
 
@@ -63,27 +71,46 @@ function getNpmPackageName(packageJsonFile) {
     theNpmPackageName = theNpmPackageName.replace("@", "").replace("/", "-");
 
     return theNpmPackageName + ".tgz";
+}
 
+function getGroupProjects() {
+    if (!fs.existsSync(packageGroupDef)) {
+        console.error("!!! Unable to locate package group definitions [" + packageGroupDef + "]");
+        throw new Error("!!! Unable to locate package group definitions.");
+    }
+
+    var groupText = removeComments(removeTrailingComma(fs.readFileSync(packageGroupDef, "utf-8")));
+
+    let groupJson = JSON.parse(groupText);
+    return groupJson[packageGroup] || [];
 }
 
 if (parseArgs()) {
-    let packageJsonFile = packageRoot + "/package.json";
+    var packages = getGroupProjects();
 
-    if (!fs.existsSync(packageJsonFile)) {
-        console.error("!!! Source package.json doesn't exist [" + packageJsonFile + "]");
-        throw new Error("!!! Source package.json doesn't exist [" + packageJsonFile + "]");
-    }
+    console.log(`Publishing [${packageGroup}] packages => ${packages.length}`);
+    packages.forEach((packageRoot) => {
+        let packageJsonFile = packageRoot + "/package.json";
 
-    let npmPackageName = packageRoot + "/" + getNpmPackageName(packageJsonFile);
-    if (!fs.existsSync(npmPackageName)) {
-        console.error("!!! NPM Package not found [" + npmPackageName + "]");
-        throw new Error("!!! NPM Package not found [" + npmPackageName + "]");
-    }
+        if (!fs.existsSync(packageJsonFile)) {
+            console.error("!!! Source package.json doesn't exist [" + packageJsonFile + "]");
+            throw new Error("!!! Source package.json doesn't exist [" + packageJsonFile + "]");
+        }
 
-    console.log(`npm pacakage present ${npmPackageName}`);
-    let npmCmd = `npm publish ${npmPackageName} --access public ${dryRun}`;
-    console.log(`Running: \"${npmCmd}\"`);
-    child_process.execSync(npmCmd);
+        console.log("\n\n##################################################################");
+        console.log("Publishing - " + getNpmPackageName(packageJsonFile));
+        console.log("##################################################################");
+        let npmPackageName = packageRoot + "/" + getNpmPackageName(packageJsonFile);
+        if (!fs.existsSync(npmPackageName)) {
+            console.error("!!! NPM Package not found [" + npmPackageName + "]");
+            throw new Error("!!! NPM Package not found [" + npmPackageName + "]");
+        }
+        
+        console.log(`npm pacakage present ${npmPackageName}`);
+        let npmCmd = `npm publish ${npmPackageName} --access public ${dryRun}`;
+        console.log(`Running: \"${npmCmd}\"`);
+        child_process.execSync(npmCmd);
+    });
 } else {
     showHelp();
     process.exit(1);
