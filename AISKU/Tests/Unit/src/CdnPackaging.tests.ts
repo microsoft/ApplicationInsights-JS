d2dbee97c8d90684fe7f6e0e0ee57297765e41ff
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2023-04-12 11:33:15 -0700

    [Release] Increase version to 3.0.0

--- AISKU/Tests/Unit/src/CdnPackaging.tests.ts
+++ AISKU/Tests/Unit/src/CdnPackaging.tests.ts
@@ -16,7 +16,7 @@ const enum CdnFormat {
 
 export class CdnPackagingChecks extends AITestClass {
     // Automatically updated by version scripts
-    private readonly currentVer = "2.8.12";
+    private readonly currentVer = "3.0.0";
 
     public testInitialize() {
     }
