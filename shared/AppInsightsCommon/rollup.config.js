de428a5e004db40e7a9a9d6f6c6cf7d348b5a14a
siyuniu-ms <siyuniu@microsoft.com>
2023-06-27 16:23:08 -0700

    update

--- shared/AppInsightsCommon/rollup.config.js
+++ shared/AppInsightsCommon/rollup.config.js
@@ -1,8 +1,13 @@
-import { createUnVersionedConfig } from "../../rollup.base.config";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import { uglify } from "@microsoft/applicationinsights-rollup-plugin-uglify3-js";
+import replace from "@rollup/plugin-replace";
+import cleanup from "rollup-plugin-cleanup";
+import { es3Poly, es3Check, importCheck } from "@microsoft/applicationinsights-rollup-es3";
+import dynamicRemove from "@microsoft/dynamicproto-js/tools/rollup/node/removedynamic";
 import { updateDistEsmFiles } from "../../tools/updateDistEsm/updateDistEsm";
 
 const version = require("./package.json").version;
-const entryPointName = "applicationinsights-common";
+const outputName = "applicationinsights-common";
 const banner = [
   "/*!",
   ` * Application Insights JavaScript SDK - Common, ${version}`,
@@ -15,22 +20,138 @@ const replaceValues = {
   "// Licensed under the MIT License.": ""
 };
 
+function doCleanup() {
+  return cleanup({
+    comments: [
+      'some', 
+      /^.\s*@DynamicProtoStub/i,
+      /^\*\*\s*@class\s*$/
+    ]
+  })
+}
 
-
-updateDistEsmFiles(replaceValues, banner, true, true, "dist-esm");
-
-export default createUnVersionedConfig(banner,
-  {
-    namespace: "Microsoft.ApplicationInsights",
-    version: version,
-    node: {
-      entryPoint: entryPointName, 
-      outputName: entryPointName
+const browserRollupConfigFactory = isProduction => {
+  const browserRollupConfig = {
+    input: `dist-esm/${outputName}.js`,
+    output: {
+      file: `browser/${outputName}.js`,
+      banner: banner,
+      format: "umd",
+      name: "Microsoft.ApplicationInsights",
+      extend: true,
+      freeze: false,
+      sourcemap: true
     },
-    browser: {
-      entryPoint: entryPointName,
-      outputName: entryPointName
+    treeshake: {
+      propertyReadSideEffects: false,
+      moduleSideEffects: false,
+      tryCatchDeoptimization: false,
+      correctVarValueBeforeDeclaration: false
     },
-  },
-  [ "applicationinsights-common" ]
-);
+    plugins: [
+      dynamicRemove(),
+      replace({
+        preventAssignment: true,
+        delimiters: ["", ""],
+        values: replaceValues
+      }),
+      importCheck({ exclude: [ "applicationinsights-common-js" ] }),
+      nodeResolve({
+        browser: false,
+        preferBuiltins: false
+      }),
+      doCleanup(),
+      es3Poly(),
+      es3Check()
+    ]
+  };
+
+  if (isProduction) {
+    browserRollupConfig.output.file = `browser/${outputName}.min.js`;
+    browserRollupConfig.plugins.push(
+      uglify({
+        ie8: true,
+        ie: true,
+        toplevel: true,
+        compress: {
+          ie: true,
+          passes:3,
+          unsafe: true
+        },
+        output: {
+          ie: true,
+          preamble: banner,
+          webkit:true
+        }
+      })
+    );
+  }
+
+  return browserRollupConfig;
+};
+
+const nodeUmdRollupConfigFactory = (isProduction) => {
+  const nodeRollupConfig = {
+    input: `dist-esm/${outputName}.js`,
+    output: {
+      file: `dist/${outputName}.js`,
+      banner: banner,
+      format: "umd",
+      name: "Microsoft.ApplicationInsights",
+      extend: true,
+      freeze: false,
+      sourcemap: true
+    },
+    treeshake: {
+      propertyReadSideEffects: false,
+      moduleSideEffects: false,
+      tryCatchDeoptimization: false,
+      correctVarValueBeforeDeclaration: false
+    },
+    plugins: [
+      dynamicRemove(),
+      replace({
+        preventAssignment: true,
+        delimiters: ["", ""],
+        values: replaceValues
+      }),
+      importCheck({ exclude: [ "applicationinsights-common-js" ] }),
+      nodeResolve(),
+      doCleanup(),
+      es3Poly(),
+      es3Check()
+    ]
+  };
+
+  if (isProduction) {
+    nodeRollupConfig.output.file = `dist/${outputName}.min.js`;
+    nodeRollupConfig.plugins.push(
+      uglify({
+        ie8: true,
+        ie: true,
+        toplevel: true,
+        compress: {
+          ie: true,
+          passes:3,
+          unsafe: true
+        },
+        output: {
+          ie: true,
+          preamble: banner,
+          webkit:true
+        }
+      })
+    );
+  }
+
+  return nodeRollupConfig;
+};
+
+updateDistEsmFiles(replaceValues, banner);
+
+export default [
+  nodeUmdRollupConfigFactory(true),
+  nodeUmdRollupConfigFactory(false),
+  browserRollupConfigFactory(true),
+  browserRollupConfigFactory(false)
+];
