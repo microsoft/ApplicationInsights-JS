ec1f4600026ad2db38be4533d8e62045ade9c44a
siyuniu-ms <siyuniu@microsoft.com>
2023-07-05 16:47:06 -0700

    add comments preserve

--- tools/applicationinsights-web-snippet/src/snippet.ts
+++ tools/applicationinsights-web-snippet/src/snippet.ts
@@ -324,8 +324,9 @@ import { IConfiguration, Snippet } from "@microsoft/applicationinsights-web";
     } else {
         // Already loaded so just call the onInit
         _onInit();
-    }
+    }// @preserve 327
 })(window, document, {
+    // @preserve config start
     src: "https://js.monitor.azure.com/scripts/b/ai.2.min.js", // The SDK URL Source
     // name: "appInsights", // Global SDK Instance name defaults to "appInsights" when not supplied
     // ld: 0, // Defines the load delay (in ms) before attempting to load the sdk. -1 = block page load and add to head. (default) = 0ms load after timeout,
@@ -334,5 +335,8 @@ import { IConfiguration, Snippet } from "@microsoft/applicationinsights-web";
     // onInit: null, // Once the application insights instance has loaded and initialized this callback function will be called with 1 argument -- the sdk instance (DO NOT ADD anything to the sdk.queue -- As they won't get called)
     cfg: { // Application Insights Configuration
         connectionString: "YOUR_CONNECTION_STRING"
+        // @preserve config end
     }
-});
+}
+// @preserve try this
+); // @preserve end here
