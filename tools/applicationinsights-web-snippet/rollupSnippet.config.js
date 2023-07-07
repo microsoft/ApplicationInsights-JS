26590042eff83f30e19a30fc66c6de0c0ba9ab57
siyuniu-ms <siyuniu@microsoft.com>
2023-07-07 15:34:18 -0700

    reduce size

--- tools/applicationinsights-web-snippet/rollupSnippet.config.js
+++ tools/applicationinsights-web-snippet/rollupSnippet.config.js
@@ -1,59 +1,20 @@
-import { es5Poly, es5Check } from "@microsoft/applicationinsights-rollup-es5";
-import replace from "@rollup/plugin-replace";
-import { uglify } from "@microsoft/applicationinsights-rollup-plugin-uglify3-js";
-import cleanup from "rollup-plugin-cleanup";
-const nodeUmdRollupConfigFactory = () => {
-  const inputName = "./dist-es5/snippet";
-  const outputName = "snippet";
-  const distPath = "./build/output/";
-  const prodOutputPath = `./build/output/snippet.min.js`;
-  const nodeRollupConfig = {
-      input: inputName,
-      output: {
-        file: `${distPath}node/${outputName}.js`,
-        name: "Microsoft.ApplicationInsights",
-          format: "umd",
-          banner: "",
-          extend: true,
-          freeze: false,
-          sourcemap: false,
-      },
-      plugins: [
-          replace({
-              preventAssignment: true
-          }),
-          cleanup({
-            comments: [
-              'some', 
-              /^.\s*@DynamicProtoStub/i,
-              /^\*\*\s*@class\s*$/
-            ]
-          }),
-          es5Poly(),
-          es5Check()
-          ]
-      };
-      nodeRollupConfig.output.file = prodOutputPath;
-      nodeRollupConfig.plugins.push(
-          uglify({
-              ie8: false,
-              ie: true,
-              toplevel: true,
-              compress: {
-                  ie: true,
-                  passes:3,
-                  unsafe: true
-              },
-              output: {
-                  ie: true,
-                  preamble: "",
-                  webkit:true,
-                  comments: [/^@preserve/]
-              }
-          })
-      );
-  return nodeRollupConfig;
-};
-export default [
-  nodeUmdRollupConfigFactory()
-];
+import { createConfig } from "../../rollup.base.config";
+
+const snippetOutputName = "snippet";
+const snippetOutputPath = "../../build/output/snippet";
+
+export default createConfig("", 
+  {
+    namespace: "Microsoft.ApplicationInsights",
+    version: "",
+    node: {
+      entryPoint: snippetOutputName, 
+      outputName: snippetOutputPath
+    },
+    browser: {
+      entryPoint: snippetOutputName, 
+      outputName: snippetOutputPath
+    },
+  },
+  [ "applicationinsights-web-snippet" ]
+);
