d7996cedc967ea1e4cc35bbc8c8556cb331fb7ac
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2023-07-28 13:07:16 -0700

    [Main] [BUG] Field 'fileName' on type 'StackFrame' is too long. Expected: 1024 characters #2086

--- channels/applicationinsights-channel-js/src/Sender.ts
+++ channels/applicationinsights-channel-js/src/Sender.ts
@@ -2,7 +2,7 @@ import dynamicProto from "@microsoft/dynamicproto-js";
 import {
     BreezeChannelIdentifier, DEFAULT_BREEZE_ENDPOINT, DEFAULT_BREEZE_PATH, DisabledPropertyName, Event, Exception, IConfig, IEnvelope,
     ISample, IStorageBuffer, Metric, PageView, PageViewPerformance, ProcessLegacy, RemoteDependencyData, RequestHeaders, SampleRate, Trace,
-    eRequestHeaders, isInternalApplicationInsightsEndpoint, utlSetStoragePrefix, utlCanUseSessionStorage
+    eRequestHeaders, isInternalApplicationInsightsEndpoint, utlCanUseSessionStorage, utlSetStoragePrefix
 } from "@microsoft/applicationinsights-common";
 import {
     BaseTelemetryPlugin, IAppInsightsCore, IChannelControls, IConfigDefaults, IConfiguration, IDiagnosticLogger, INotificationManager,
