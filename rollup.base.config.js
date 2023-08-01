1399417f202628bc5a7e0332e7ff2916096ba9d3
siyuniu-ms <siyuniu@microsoft.com>
2023-08-01 16:28:27 -0700

    Update rollup.base.config.js

--- rollup.base.config.js
+++ rollup.base.config.js
@@ -296,6 +296,17 @@ export function createConfig(banner, cfg, importCheckNames, replaceValues, major
         let browserPostfix = browserCfg.postfix || '';
         let browserTeam = browserCfg.teamExt || '';
 
+        console.log("----------cfg version", cfg.version);
+
+        if (cfg.version) {
+            var version = cfg.version.split(".");
+            var majorVer = version[0].trim();
+            if (majorVer && !browserNamespace.browser.endsWith(majorVer)) {
+                browserNamespace.browser += majorVer;
+            }
+            browserNamespace.ver = cfg.version;
+        }
+
         if (majorVersionSet){
             tasks.push(
                 browserRollupConfigFactory(banner, importCheckNames, targetType, browserNamespace, cfg.browser.entryPoint, cfg.browser.outputName, majorVersion, true, browserFmt, browserPostfix, browserTeam, replaceValues, treeshakeConfig),
