0924a0ecf614a6e18b843de1ab3f3d1c756ef9c9
siyuniu-ms <siyuniu@microsoft.com>
2023-08-28 16:08:37 -0700

    Update type.ts

--- tools/applicationinsights-web-snippet/src/type.ts
+++ tools/applicationinsights-web-snippet/src/type.ts
@@ -8,7 +8,7 @@ export interface ISnippetConfig {
     crossOrigin?: string;
     onInit?: any;
     cfg: IConfiguration;
-    cr?: boolean;
+    cr?: boolean; // cdn retry would be proceed if ture
 }
 
 export interface Fields {
