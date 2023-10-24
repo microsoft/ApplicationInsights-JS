8de53ec7b4c42f0d1b4205ac5e008acf6b973ad7
siyuniu-ms <siyuniu@microsoft.com>
2023-10-24 16:18:42 -0700

    Update HttpManager.ts

--- channels/1ds-post-js/src/HttpManager.ts
+++ channels/1ds-post-js/src/HttpManager.ts
@@ -470,6 +470,23 @@ export class HttpManager {
                     requestInit.headers = payload.headers;
                 }
 
+                const handleResponse = (status, headerMap, responseText) => {
+                    if (!responseHandled) {
+                        responseHandled = true;
+                        _doOnComplete(oncomplete, status, headerMap, responseText);
+                        _handleCollectorResponse(responseText);
+                    }
+                };
+            
+                const handleError = (error) => {
+                    // In case there is an error in the request. Set the status to 0
+                    // so that the events can be retried later.
+                    if (!responseHandled) {
+                            responseHandled = true;
+                            _doOnComplete(oncomplete, 0, {});
+                        }
+                    };
+
                 fetch(theUrl, requestInit).then((response) => {
                     let headerMap = {};
                     let responseText = STR_EMPTY;
@@ -482,27 +499,12 @@ export class HttpManager {
                     if (response.body) {
                         response.text().then(function(text) {
                             responseText = text;
-                            if (!responseHandled) {
-                                responseHandled = true;
-                                _doOnComplete(oncomplete, response.status, headerMap, responseText);
-                                _handleCollectorResponse(responseText);
-                            }
-                        });
+                            handleResponse(response.status, headerMap, responseText);
+                        }, handleError);
                     } else {
-                        if (!responseHandled) {
-                            responseHandled = true;
-                            _doOnComplete(oncomplete, 0, {});
-                        }
+                        handleResponse(response.status, headerMap, "");
                     }
-                  
-                }).catch((error) => {
-                    // In case there is an error in the request. Set the status to 0
-                    // so that the events can be retried later.
-                    if (!responseHandled) {
-                        responseHandled = true;
-                        _doOnComplete(oncomplete, 0, {});
-                    }
-                });
+                }).catch(handleError);
 
                 if (ignoreResponse && !responseHandled) {
                     // Assume success during unload processing
