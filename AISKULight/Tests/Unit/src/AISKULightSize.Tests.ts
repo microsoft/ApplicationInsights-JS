9dda05ca6728228c11acdd64513042913bc67fea
siyuniu-ms <123212536+siyuniu-ms@users.noreply.github.com>
2023-11-01 15:55:27 -0700

    [Main][Release] Increase version to 3.0.5 (#2192)

--- AISKULight/Tests/Unit/src/AISKULightSize.Tests.ts
+++ AISKULight/Tests/Unit/src/AISKULightSize.Tests.ts
@@ -7,7 +7,7 @@ export class AISKULightSizeCheck extends AITestClass {
     private readonly MAX_RAW_DEFLATE_SIZE = 33;
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 33;
     private readonly rawFilePath = "../dist/es5/applicationinsights-web-basic.min.js";
-    private readonly currentVer = "3.0.4";
+    private readonly currentVer = "3.0.5";
     private readonly prodFilePath = `../browser/es5/aib.${this.currentVer[0]}.min.js`;
 
     public testInitialize() {
