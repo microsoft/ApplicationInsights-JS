ed34fa2d913fb4554452f6bfa92fd8454efd19b1
siyuniu-ms <siyuniu@microsoft.com>
2023-11-20 11:19:48 -0800

    Update AISKULightSize.Tests.ts

--- AISKULight/Tests/Unit/src/AISKULightSize.Tests.ts
+++ AISKULight/Tests/Unit/src/AISKULightSize.Tests.ts
@@ -2,8 +2,8 @@ import { AITestClass, Assert } from "@microsoft/ai-test-framework";
 import * as pako from "pako";
 
 export class AISKULightSizeCheck extends AITestClass {
-    private readonly MAX_RAW_SIZE = 81;
-    private readonly MAX_BUNDLE_SIZE = 81;
+    private readonly MAX_RAW_SIZE = 82;
+    private readonly MAX_BUNDLE_SIZE = 82;
     private readonly MAX_RAW_DEFLATE_SIZE = 34;
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 34;
     private readonly rawFilePath = "../dist/es5/applicationinsights-web-basic.min.js";
