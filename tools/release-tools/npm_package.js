c5204729e549fcfebb81d34ba11f20e0083297e4
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2024-01-17 15:48:42 -0800

    Internal Task: Update npm pack sequence

--- tools/release-tools/npm_package.js
+++ tools/release-tools/npm_package.js
@@ -3,6 +3,7 @@ const child_process = require("child_process");
 
 const packageGroupDef = "./tools/release-tools/package_groups.json";
 let packageGroup;
+let dropFolder;
 let dryRun = "";
 
 function showHelp() {
@@ -19,10 +20,12 @@ function showHelp() {
     console.log("");
     console.log(scriptName + " <group> ");
     console.log("--------------------------");
-    console.log(" <group>      - Identifies the group to publish, identifies folders");
+    console.log(" <group>      - Identifies the group to publish, identifies folders, the group must be defined in package_groups.json");
+    console.log(" <dropFolder> - Identifies the base folder to drop the packages into, defaults to ./drop/packages/<group>");
 }
 
 function parseArgs() {
+    console.log("Parsing args - " + process.argv.join(" "));
     if (process.argv.length < 2) {
         console.error("!!! Invalid number of arguments -- " + process.argv.length);
         return false;
@@ -40,6 +43,8 @@ function parseArgs() {
             }
         } else if (!packageGroup) {
             packageGroup = theArg;
+        } else if (!dropFolder) {
+            dropFolder = theArg;
         } else {
             console.error("!!! Invalid Argument [" + theArg + "] detected");
             return false;
@@ -65,10 +70,13 @@ function removeComments(text) {
     return text.replace(/^\s*\/\/\s.*$/gm, "");
 }
 
-function getNpmPackageName(packageJsonFile) {
+function getPackage(packageJsonFile) {
     var packageText = removeTrailingComma(fs.readFileSync(packageJsonFile, "utf-8"));
 
-    let packageJson = JSON.parse(packageText);
+    return JSON.parse(packageText);
+}
+
+function getNpmPackageName(packageJson) {
     let packageName = packageJson.name;
     let packageVersion = packageJson.version;
 
@@ -91,10 +99,31 @@ function getGroupProjects() {
     return groupJson[packageGroup] || [];
 }
 
+function movePackage(npmPackageName, packageName) {
+    let packageFolder = dropFolder;
+    if (!packageFolder) {
+        packageFolder = "./drop/packages";
+        packageFolder += "/" + packageGroup;
+    }
+
+    if (!fs.existsSync(packageFolder)) {
+        fs.mkdirSync(packageFolder, { recursive: true });
+    }
+
+    let packageFile = packageFolder + "/" + packageName;
+    if (fs.existsSync(packageFile)) {
+        console.log(` -- Removing existing package ${packageFile}`);
+        fs.unlinkSync(packageFile);
+    }
+
+    console.log(` -- Moving ${npmPackageName} to ${packageFile}`);
+    fs.renameSync(npmPackageName, packageFile);
+}
+
 if (parseArgs()) {
     var packages = getGroupProjects();
 
-    console.log(`Publishing [${packageGroup}] packages => ${packages.length}`);
+    console.log(`Creating [${packageGroup}] packages => ${packages.length}`);
     packages.forEach((packageRoot) => {
         let packageJsonFile = packageRoot + "/package.json";
 
@@ -103,19 +132,33 @@ if (parseArgs()) {
             throw new Error("!!! Source package.json doesn't exist [" + packageJsonFile + "]");
         }
 
+        const packageJson = getPackage(packageJsonFile);
+
+        const packageName = getNpmPackageName(packageJson);
         console.log("\n\n##################################################################");
-        console.log("Publishing - " + getNpmPackageName(packageJsonFile));
+        console.log("Packaging - " + packageName);
         console.log("##################################################################");
-        let npmPackageName = packageRoot + "/" + getNpmPackageName(packageJsonFile);
-        if (!fs.existsSync(npmPackageName)) {
-            console.error("!!! NPM Package not found [" + npmPackageName + "]");
-            throw new Error("!!! NPM Package not found [" + npmPackageName + "]");
+
+        let npmPackageName = packageRoot + "/" + packageName;
+        if (fs.existsSync(npmPackageName)) {
+            console.log(` -- Removing existing package ${npmPackageName}`);
+            fs.unlinkSync(npmPackageName);
         }
-        
-        console.log(`npm package present ${npmPackageName}`);
-        let npmCmd = `npm publish ${npmPackageName} --access public ${dryRun}`;
-        console.log(`Running: \"${npmCmd}\"`);
-        child_process.execSync(npmCmd);
+    
+        const cwd = process.cwd();
+        process.chdir(packageRoot);
+        try {
+            let npmCmd = `npm pack ${dryRun}`;
+            console.log(`Running: \"${npmCmd}\"`);
+            child_process.execSync(npmCmd);
+        } finally {
+            process.chdir(cwd);
+        }
+
+        if (!dryRun) {
+            // Move the package to the package folder
+            movePackage(npmPackageName, packageName);
+        }
     });
 } else {
     showHelp();
