513119d79d4c6c6f633ed20923b89af0f6f3cd4a
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2024-03-12 14:28:10 -0700

    [Release-3.0] [Release] Increase version to 3.0.9

--- gruntfile.js
+++ gruntfile.js
@@ -239,7 +239,7 @@ module.exports = function (grunt) {
     // const perfTestVersions = ["2.0.0","2.0.1","2.1.0","2.2.0","2.2.1","2.2.2","2.3.0","2.3.1",
     // "2.4.1","2.4.3","2.4.4","2.5.2","2.5.3","2.5.4","2.5.5","2.5.6","2.5.7","2.5.8","2.5.9","2.5.10","2.5.11",
     // "2.6.0","2.6.1","2.6.2","2.6.3","2.6.4","2.6.5","2.7.0"];
-    const perfTestVersions=["3.0.8"];
+    const perfTestVersions=["3.0.9"];
 
     function buildConfig(modules) {
         var buildCmds = {
