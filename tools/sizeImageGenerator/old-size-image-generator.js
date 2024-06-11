e0c10815260635fa0ce095bba2a3ed26380d5842
siyuniu-ms <siyuniu@microsoft.com>
2024-06-11 10:09:01 -0700

    add az ver also

--- tools/sizeImageGenerator/old-size-image-generator.js
+++ tools/sizeImageGenerator/old-size-image-generator.js
@@ -39,7 +39,6 @@ function createDirectory(dirName) {
 
 async function main() {
     createDirectory("cdnFile");
-    console.log("created cdnFile directory")
     createDirectory("img");
     const versions = ["3.1.2", "3.1.1", "3.1.0", "3.0.9", "3.0.8", "3.0.7", 
     "3.0.6", "3.0.5", "3.0.4", "3.0.3", "3.0.2", "3.0.1", "3.0.0", "2.8.18", 
