5438a6d75148ff621a7351f0b4d1083ca0aa367b
Karlie-777 <79606506+Karlie-777@users.noreply.github.com>
2024-08-30 17:34:54 -0700

    [Main] [Release] Increase version to 3.3.2 (#2400)
    * release 3.3.2
    
    * update
    
    * update

--- AISKU/Tests/Unit/src/CdnPackaging.tests.ts
+++ AISKU/Tests/Unit/src/CdnPackaging.tests.ts
@@ -16,7 +16,7 @@ const enum CdnFormat {
 
 export class CdnPackagingChecks extends AITestClass {
     // Automatically updated by version scripts
-    private readonly currentVer = "3.3.1";
+    private readonly currentVer = "3.3.2";
 
     public testInitialize() {
     }
