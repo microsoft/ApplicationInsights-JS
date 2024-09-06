31da7d8063b24cdbaa0eb7c1d61a3ed1b3b13089
siyuniu-ms <siyuniu@microsoft.com>
2024-09-06 13:31:53 -0700

    Update IExceptionConfig.ts

--- shared/AppInsightsCore/src/JavaScriptSDK.Interfaces/IExceptionConfig.ts
+++ shared/AppInsightsCore/src/JavaScriptSDK.Interfaces/IExceptionConfig.ts
@@ -1,14 +1,37 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/**
+ * Configuration for extra exceptions information sent with the exception telemetry.
+ * @example
+ * ```js
+ * const appInsights = new ApplicationInsights({
+      config: {
+          connectionString: 'InstrumentationKey=YOUR_INSTRUMENTATION_KEY_GOES_HERE',
+          expCfg: {
+              inclScripts: true,
+              expLog : () => {
+                  return {logs: ["log info 1", "log info 2"]};
+              },
+              maxLogs : 100
+          }
+      }
+  });
+  appInsights.trackException({error: new Error(), severityLevel: SeverityLevel.Critical});
+* ```
+ * @interface IExceptionConfig
+ */
 export interface IExceptionConfig{
     /**
      * If set to true, when exception is sent out, the SDK will also send out all scripts basic info that are loaded on the page.
      * Notice: This would increase the size of the exception telemetry.
+     * @type {boolean}
+     * @memberof IExceptionConfig
+     * @defaultvalue true
      */
     inclScripts?: boolean;
 
-   /**
+    /**
      * Callback function for collecting logs to be included in telemetry data.
      *
      * The length of logs to generate is controlled by the `maxLogs` parameter.
@@ -20,6 +43,14 @@ export interface IExceptionConfig{
      *
      * @property {number} maxLogs - Specifies the maximum number of logs that can be generated. If not explicitly set, it defaults to 50.
      */
-    expLog: () => { logs: string[] },
-    maxLogs: number
+    expLog?: () => { logs: string[] },
+
+    /**
+     * The maximum number of logs to include in the telemetry data.
+     * If not explicitly set, it defaults to 50.
+     * This is used in conjunction with the `expLog` callback.
+     * @type {number}
+     * @memberof IExceptionConfig
+     */
+    maxLogs?: number
 }
