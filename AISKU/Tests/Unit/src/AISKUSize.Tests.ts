e31674433ce23328ad53d9d570bcd46afce349ab
Nev Wylie <54870357+MSNev@users.noreply.github.com>
2024-09-23 20:26:14 -0700

    [Main]: Make file size checks flexible for nightly/dev builds

--- AISKU/Tests/Unit/src/AISKUSize.Tests.ts
+++ AISKU/Tests/Unit/src/AISKUSize.Tests.ts
@@ -1,8 +1,57 @@
 import { AITestClass, Assert } from "@microsoft/ai-test-framework";
+import { dumpObj } from '@nevware21/ts-utils';
+import { createPromise, doAwait, IPromise } from '@nevware21/ts-async';
 import { strUndefined } from "@microsoft/applicationinsights-core-js";
+import { utlRemoveSessionStorage } from "@microsoft/applicationinsights-common";
 import * as pako from "pako";
 import { Snippet } from "../../../src/Snippet";
-import { utlRemoveSessionStorage } from "@microsoft/applicationinsights-common";
+
+const PACKAGE_JSON = "../package.json";
+
+function removeTrailingComma(text) {
+    return text.replace(/,(\s*[}\],])/g, "$1");
+}
+
+function _loadPackageJson(cb:(isNightly: boolean, packageJson: any) => IPromise<void>): IPromise<void> {
+    return createPromise<void>((testCompleted, testFailed) => {
+        function _handleCallback(packageJson: any) {
+            let version = packageJson.version || "unknown";
+            let isNightly = version.includes("nightly") || version.includes("dev");
+            doAwait(cb(isNightly, packageJson), () => {
+                testCompleted();
+            }, (error) => {
+                Assert.ok(false, `checkIsNightlyBuild error: ${error}`);
+                testFailed(error);
+            });
+        }
+
+        fetch(PACKAGE_JSON).then((response) => {
+            if (!response.ok) {
+                Assert.ok(false, `fetch package.json error: ${dumpObj(response)}`);
+                _handleCallback(false);
+            } else {
+                return response.text().then((content) => {
+                    let json = JSON.parse(removeTrailingComma(content));
+                    _handleCallback(json);
+                }, (error) => {
+                    Assert.ok(false, `fetch package.json error: ${error}`);
+                    _handleCallback({});
+                });
+            }
+        }, (error) => {
+            Assert.ok(false, `fetch package.json error: ${error}`);
+            _handleCallback({});
+        });
+    });
+}
+
+function _checkSize(checkType: string, maxSize: number, size: number, isNightly: boolean): void {
+  if (isNightly) {
+        maxSize += .5;
+    }
+
+    Assert.ok(size <= maxSize, `exceed ${maxSize} KB, current ${checkType} size is: ${size} KB`);
+}    
 
 export class AISKUSizeCheck extends AITestClass {
     private readonly MAX_RAW_SIZE = 144;
@@ -67,23 +116,27 @@ export class AISKUSizeCheck extends AITestClass {
             name: `Test AISKU${postfix} deflate size`,
             test: () => {
                 Assert.ok(true, `test file: ${fileName}`);
-                let request = new Request(_filePath, {method:"GET"});
-                return fetch(request).then((response) => {
-                    if (!response.ok) {
-                        Assert.ok(false, `fetch AISKU${postfix} error: ${response.statusText}`);
-                        return;
-                    } else {
-                        return response.text().then(text => {
-                            let size = Math.ceil((text.length/1024) * 100) / 100.0;
-                            Assert.ok(size <= _maxFullSize, `max ${_maxFullSize} KB, current deflate size is: ${size} KB`);
-                            let deflateSize = Math.ceil((pako.deflate(text).length/1024) * 100) / 100.0;
-                            Assert.ok(deflateSize <= _maxDeflateSize ,`max ${_maxDeflateSize} KB, current deflate size is: ${deflateSize} KB`);
-                        }).catch((error: Error) => {
-                            Assert.ok(false, `AISKU${postfix} response error: ${error}`);
-                        });
-                    }
-                }).catch((error: Error) => {
-                    Assert.ok(false, `AISKU${postfix} deflate size error: ${error}`);
+                return _loadPackageJson((isNightly, packageJson) => {
+                    Assert.ok(true, `  checking : ${packageJson.name || "??"} v${packageJson.version || "unknown"}`);
+                    let request = new Request(_filePath, {method:"GET"});
+                    return fetch(request).then((response) => {
+                        if (!response.ok) {
+                            Assert.ok(false, `fetch AISKU${postfix} error: ${response.statusText}`);
+                            return;
+                        } else {
+                            return response.text().then(text => {
+                                let size = Math.ceil((text.length/1024) * 100) / 100.0;
+                                _checkSize("bundle", _maxFullSize, size, isNightly);
+                                
+                                let deflateSize = Math.ceil((pako.deflate(text).length/1024) * 100) / 100.0;
+                                _checkSize("deflate", _maxDeflateSize, deflateSize, isNightly);
+                            }).catch((error: Error) => {
+                                Assert.ok(false, `AISKU${postfix} response error: ${error}`);
+                            });
+                        }
+                    }).catch((error: Error) => {
+                        Assert.ok(false, `AISKU${postfix} deflate size error: ${error}`);
+                    });
                 });
             }
         });
--- extensions/applicationinsights-properties-js/Tests/Unit/src/propertiesSize.tests.ts
+++ extensions/applicationinsights-properties-js/Tests/Unit/src/propertiesSize.tests.ts
@@ -1,5 +1,54 @@
 import { Assert, AITestClass } from "@microsoft/ai-test-framework";
+import { dumpObj } from '@nevware21/ts-utils';
+import { createPromise, doAwait, IPromise } from '@nevware21/ts-async';
 import * as pako from "pako";
+
+const PACKAGE_JSON = "../package.json";
+
+function removeTrailingComma(text) {
+    return text.replace(/,(\s*[}\],])/g, "$1");
+}
+
+function _loadPackageJson(cb:(isNightly: boolean, packageJson: any) => IPromise<void>): IPromise<void> {
+    return createPromise<void>((testCompleted, testFailed) => {
+        function _handleCallback(packageJson: any) {
+            let version = packageJson.version || "unknown";
+            let isNightly = version.includes("nightly") || version.includes("dev");
+            doAwait(cb(isNightly, packageJson), () => {
+                testCompleted();
+            }, (error) => {
+                Assert.ok(false, `checkIsNightlyBuild error: ${error}`);
+                testFailed(error);
+            });
+        }
+
+        fetch(PACKAGE_JSON).then((response) => {
+            if (!response.ok) {
+                Assert.ok(false, `fetch package.json error: ${dumpObj(response)}`);
+                _handleCallback(false);
+            } else {
+                return response.text().then((content) => {
+                    let json = JSON.parse(removeTrailingComma(content));
+                    _handleCallback(json);
+                }, (error) => {
+                    Assert.ok(false, `fetch package.json error: ${error}`);
+                    _handleCallback({});
+                });
+            }
+        }, (error) => {
+            Assert.ok(false, `fetch package.json error: ${error}`);
+            _handleCallback({});
+        });
+    });
+}
+
+function _checkSize(checkType: string, maxSize: number, size: number, isNightly: boolean): void {
+  if (isNightly) {
+        maxSize += .5;
+    }
+
+    Assert.ok(size <= maxSize, `exceed ${maxSize} KB, current ${checkType} size is: ${size} KB`);
+}    
 
 export class PropertiesExtensionSizeCheck extends AITestClass {
     private readonly MAX_DEFLATE_SIZE = 18;
@@ -39,21 +88,24 @@ export class PropertiesExtensionSizeCheck extends AITestClass {
             name: `Test applicationinsights-properties${postfix} deflate size`,
             test: () => {
                 Assert.ok(true, `test file: ${fileName}`);
-                let request = new Request(_filePath, {method:"GET"});
-                return fetch(request).then((response) => {
-                    if (!response.ok) {
-                        Assert.ok(false, `fetch applicationinsights-properties${postfix} response error: ${response.statusText}`);
-                        return;
-                    } else {
-                        return response.text().then(text => {
-                            let size = Math.ceil((pako.deflate(text).length/1024) * 100) / 100.0;
-                            Assert.ok(size <= this.MAX_DEFLATE_SIZE ,`max ${this.MAX_DEFLATE_SIZE} KB, current deflate size is: ${size} KB`);
-                        }).catch((error) => {
-                            Assert.ok(false, `applicationinsights-properties${postfix} response error: ${error}`);
-                        });
-                    }
-                }).catch((error: Error) => {
-                    Assert.ok(false, `applicationinsights-properties${postfix} deflate size error: ${error}`);
+                return _loadPackageJson((isNightly, packageJson) => {
+                    Assert.ok(true, `  checking : ${packageJson.name || "??"} v${packageJson.version || "unknown"}`);
+                    let request = new Request(_filePath, {method:"GET"});
+                    return fetch(request).then((response) => {
+                        if (!response.ok) {
+                            Assert.ok(false, `fetch applicationinsights-properties${postfix} response error: ${response.statusText}`);
+                            return;
+                        } else {
+                            return response.text().then(text => {
+                                let size = Math.ceil((pako.deflate(text).length/1024) * 100) / 100.0;
+                                _checkSize("deflate", this.MAX_DEFLATE_SIZE, size, isNightly);
+                            }).catch((error) => {
+                                Assert.ok(false, `applicationinsights-properties${postfix} response error: ${error}`);
+                            });
+                        }
+                    }).catch((error: Error) => {
+                        Assert.ok(false, `applicationinsights-properties${postfix} deflate size error: ${error}`);
+                    });
                 });
              }
         });
