3cfb7ac4ee621ed47e61807665ddf5584f1520d3
siyuniu-ms <123212536+siyuniu-ms@users.noreply.github.com>
2024-11-06 15:35:29 -0800

    [main] [snippet] Integrate 1DS with AI Snippet Generation (#2428)

---
+++ tools/applicationinsights-web-snippet/rollupOneDSSnippet.config.js
@@ -0,0 +1,19 @@
+import { createUnVersionedConfig } from "../../rollup.base.config";
+
+const snippetOutputName = "oneDSSnippet";
+const snippetOutputPath = "../../build/output/oneDSSnippet";
+
+export default createUnVersionedConfig("", 
+  {
+    namespace: "oneDS",
+    version: "",
+    browser: {
+      entryPoint: snippetOutputName, 
+      outputName: snippetOutputPath,
+      inputPath: "build/output",
+      formats: [{ format: 'cjs', postfix: '', useStrict: false, topLevel: true }],
+    },
+  },
+  [ "applicationinsights-web-snippet" ],
+  false
+);
---
+++ tools/applicationinsights-web-snippet/src/aiSupport.ts
@@ -0,0 +1,59 @@
+import { IEnvelope } from "@microsoft/applicationinsights-common";
+import { _getTime } from "./support";
+
+let strEmpty = "";
+
+
+
+export function _createAiEnvelope(iKey: string, theType: string, sv: string, version: number, locn: Location): IEnvelope {
+    let tags = {};
+    let type = "Browser";
+    let strAiDevice = "ai.device.";
+    let strAiOperationName = "ai.operation.name";
+    let strAiSdkVersion = "ai.internal.sdkVersion";
+    let strToLowerCase = "toLowerCase";
+    tags[strAiDevice + "id"] = type[strToLowerCase]();
+    tags[strAiDevice + "type"] = type;
+    tags[strAiOperationName] = locn && locn.pathname || "_unknown_";
+    tags[strAiSdkVersion] = "javascript:snippet_" + (sv || version);
+
+    let envelope:IEnvelope = {
+        time: _getTime(),
+        iKey: iKey,
+        name: "Microsoft.ApplicationInsights." + iKey.replace(/-/g, strEmpty) + "." + theType,
+        sampleRate: 100,
+        tags: tags,
+        data: {
+            baseData: {
+                ver: 2
+            }
+        },
+        ver: undefined,
+        seq: "1",
+        aiDataContract: undefined
+    };
+    return envelope;
+}
+
+let track = "track";
+let trackPage = "TrackPage";
+let trackEvent = "TrackEvent";
+
+export const aiMethod = [
+    track + "Event",
+    track + "Exception",
+    trackPage + "View",
+    trackPage + "ViewPerformance",
+    "addTelemetryInitializer",
+    track + "Trace",
+    track + "DependencyData",
+    track + "Metric",
+    "start" + trackPage,
+    "stop" + trackPage,
+    "start" + trackEvent,
+    "stop" + trackEvent,
+    "setAuthenticatedUserContext",
+    "clearAuthenticatedUserContext",
+    "flush"
+]
+
