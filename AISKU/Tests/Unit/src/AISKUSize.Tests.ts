eaf87625e3c03bfffc3c183aec88091e175ef0ca
siyuniu-ms <siyuniu@microsoft.com>
2025-04-29 15:14:22 -0700

    Update AISKUSize.Tests.ts

--- AISKU/Tests/Unit/src/AISKUSize.Tests.ts
+++ AISKU/Tests/Unit/src/AISKUSize.Tests.ts
@@ -54,8 +54,8 @@ function _checkSize(checkType: string, maxSize: number, size: number, isNightly:
 }    
 
 export class AISKUSizeCheck extends AITestClass {
-    private readonly MAX_RAW_SIZE = 146;
-    private readonly MAX_BUNDLE_SIZE = 146;
+    private readonly MAX_RAW_SIZE = 147;
+    private readonly MAX_BUNDLE_SIZE = 147;
     private readonly MAX_RAW_DEFLATE_SIZE = 59;
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 59;
     private readonly rawFilePath = "../dist/es5/applicationinsights-web.min.js";
