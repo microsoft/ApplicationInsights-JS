1b55833bc6d0b5349952699d297389b3710ae290
Radhika Gupta <guptaradhika+microsoft@microsoft.com>
2025-05-12 16:55:51 -0700

    Increased the size for AppInsightsCore

--- shared/AppInsightsCore/Tests/Unit/src/AppInsightsCoreSize.Tests.ts
+++ shared/AppInsightsCore/Tests/Unit/src/AppInsightsCoreSize.Tests.ts
@@ -51,8 +51,8 @@ function _checkSize(checkType: string, maxSize: number, size: number, isNightly:
 }    
 
 export class AppInsightsCoreSizeCheck extends AITestClass {
-    private readonly MAX_RAW_SIZE = 68
-    private readonly MAX_BUNDLE_SIZE = 68
+    private readonly MAX_RAW_SIZE = 70
+    private readonly MAX_BUNDLE_SIZE = 70
     private readonly MAX_RAW_DEFLATE_SIZE = 28;
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 28;
     private readonly rawFilePath = "../dist/es5/applicationinsights-core-js.min.js";
