4835556ce61ba902e4da0b85e76d7f175a39ffea
Karlie-777 <79606506+Karlie-777@users.noreply.github.com>
2025-09-22 15:52:06 -0700

    [Release] Increase version to 3.3.10 (#2650)
    * release 3.3.10
    
    * update

--- AISKU/Tests/Unit/src/AISKUSize.Tests.ts
+++ AISKU/Tests/Unit/src/AISKUSize.Tests.ts
@@ -60,7 +60,7 @@ export class AISKUSizeCheck extends AITestClass {
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 60;
     private readonly rawFilePath = "../dist/es5/applicationinsights-web.min.js";
     // Automatically updated by version scripts
-    private readonly currentVer = "3.3.9";
+    private readonly currentVer = "3.3.10";
     private readonly prodFilePath = `../browser/es5/ai.${this.currentVer[0]}.min.js`;
 
     public testInitialize() {
