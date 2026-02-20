58828a2802346177d4901e5142e222b5fbe82e56
Nev <54870357+MSNev@users.noreply.github.com>
2026-02-20 15:11:42 -0800

    [Beta Release] Increase version to 3.4.0-beta

--- AISKU/Tests/Unit/src/AISKUSize.Tests.ts
+++ AISKU/Tests/Unit/src/AISKUSize.Tests.ts
@@ -60,7 +60,7 @@ export class AISKUSizeCheck extends AITestClass {
     private readonly MAX_BUNDLE_DEFLATE_SIZE = 70;
     private readonly rawFilePath = "../dist/es5/applicationinsights-web.min.js";
     // Automatically updated by version scripts
-    private readonly currentVer = "3.3.11";
+    private readonly currentVer = "3.4.0-beta";
     private readonly prodFilePath = `../browser/es5/ai.${this.currentVer[0]}.min.js`;
 
     public testInitialize() {
--- RELEASES.md
+++ RELEASES.md
@@ -2,7 +2,7 @@
 
 > Note: ES3/IE8 compatibility will be removed in the future v3.x.x releases (scheduled for mid-late 2022), so if you need to retain ES3 compatibility you will need to remain on the 2.x.x versions of the SDK or your runtime will need install polyfill's to your ES3 environment before loading / initializing the SDK.
 
-## 3.4.0-beta (Unreleased)
+## 3.4.0-beta (February 23rd, 2026)
 
 ### Significant Changes
 
